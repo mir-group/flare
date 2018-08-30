45df57cd91e78bfab23e9e4d75ad48b25fde11e7
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2018-08-30 19:44:19 -0400

    add public repository

---
+++ gp.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""" Gaussian Process Regression model
+
+Implementation is based on Algorithm 2.1 (pg. 19) of
+"Gaussian Processes for Machine Learning" by Rasmussen and Williams
+
+Simon Batzner
+"""
+import os
+
+import math
+
+import numpy as np
+from scipy.linalg import solve_triangular
+from scipy.optimize import minimize
+
+from two_body import two_body
+from kern_help import get_envs
+from otf import parse_qe_input, parse_qe_forces, Structure
+from MD_Parser import parse_qe_pwscf_md_output
+
+
+def get_outfiles(root_dir, out=True):
+    """
+    Find all files matching *.out or *.in
+
+    :param root_dir:    (str), dir to walk from
+    :param out:         (bool), whether to look for .out or .in files
+    :return: matching   (list), files in root_dir ending with .out
+    """
+    matching = []
+
+    for root, _, filenames in os.walk(root_dir):
+        for filename in filenames:
+
+            if out:
+                if filename.endswith('.out'):
+                    matching.append(os.path.join(root, filename))
+
+            else:
+                if filename.endswith('.in'):
+                    matching.append(os.path.join(root, filename))
+
+    return matching
+
+
+class GaussianProcess:
+    """
+    Gaussian Process Regression Model
+    """
+
+    def __init__(self, kernel):
+        """
+        Initialize GP parameters and training data
+
+        :param: kernel  (func) func specifying used GP kernel
+        """
+
+        # predictive mean and variance
+        self.pred_mean = None
+        self.pred_var = None
+
+        # gp kernel and hyperparameters
+        self.kernel = kernel
+        self.length_scale = None
+        self.sigma_n = None
+        self.sigma_f = None
+
+        # quantities used in GPR algorithm
+        self.k_mat = None
+        self.l_mat = None
+        self.alpha = None
+
+        # training set
+        self.training_data = np.empty(0,)
+        self.training_labels = np.empty(0,)
+
+    def init_db(self, root_dir, brav_mat, brav_inv,
+                vec1, vec2, vec3, cutoff):
+        """Initialize database from root directory containing training data"""
+        pos, _, _, frcs = [], [], [], []
+
+        for file in get_outfiles(root_dir=root_dir, out=False):
+            pos = parse_qe_input(file)[0]
+
+        for file in get_outfiles(root_dir=root_dir, out=True):
+            frcs = parse_qe_forces(file)
+
+        self.training_data = np.asarray(get_envs(pos=pos,
+                                                 typs=['Si'],
+                                                 brav_mat=brav_mat,
+                                                 brav_inv=brav_inv,
+                                                 vec1=vec1,
+                                                 vec2=vec2,
+                                                 vec3=vec3,
+                                                 cutoff=cutoff))
+        self.training_labels = np.asarray(frcs)
+
+    def train(self):
+        """
+        Train Gaussian Process model on training data
+        """
+
+        # optimize hyperparameters
+        self.opt_hyper()
+
+        # following: Algorithm 2.1 (pg. 19) of
+        # "Gaussian Processes for Machine Learning" by Rasmussen and Williams
+        self.set_kernel(sigma_f=self.sigma_f,
+                        length_scale=self.length_scale,
+                        sigma_n=self.sigma_n)
+
+        # get alpha and likelihood
+        self.set_alpha()
+
+    def opt_hyper(self):
+        """
+        Optimize hyperparameters of GP by minimizing minus log likelihood
+        """
+        # initial guess
+        x_0 = np.array([self.sigma_f, self.length_scale, self.sigma_n])
+
+        # nelder-mead optimization
+        res = minimize(self.minus_like_hyp, x_0, method='nelder-mead',
+                       options={'xtol': 1e-8, 'disp': True})
+
+        self.sigma_f = res.x[0]
+        self.length_scale = res.x[1]
+        self.sigma_n = res.x[2]
+
+    def predict(self, x_t, d):
+        """ Make GP prediction with SE kernel """
+
+        # get kernel vector
+        k_v = self.get_kernel_vector(x=x_t, d_1=1)
+
+        # get predictive mean
+        self.pred_mean = np.matmul(k_v.transpose(), self.alpha)
+
+        # get predictive variance
+        v_vec = solve_triangular(self.l_mat, k_v, lower=True)
+        self_kern = self.kernel(x_t, x_t, d, d,
+                                self.sigma_f,
+                                self.length_scale)
+        self.pred_var = self_kern - np.matmul(v_vec.transpose(), v_vec)
+
+    def minus_like_hyp(self, hyp):
+        """
+        Get minus likelihood as a function of hyperparameters
+
+        :param hyp          list of hyperparameters to optimize
+        :return minus_like  negative likelihood
+        """
+        like = self.like_hyp(hyp)
+        minus_like = -like
+
+        return minus_like
+
+    def like_hyp(self, hyp):
+        """
+        Get likelihood as a function of hyperparameters
+
+        :param  hyp      hyperparameters to be optimized
+        :return like    likelihood
+        """
+
+        # unpack hyperparameters
+        sigma_f = hyp[0]
+        length_scale = hyp[1]
+        sigma_n = hyp[2]
+
+        # calculate likelihood
+        self.set_kernel(sigma_f=sigma_f,
+                        length_scale=length_scale,
+                        sigma_n=sigma_n)
+        self.set_alpha()
+        like = self.get_likelihood()
+
+        return like
+
+    def get_likelihood(self):
+        """
+        Get log marginal likelihood
+
+        :return like    likelihood
+        """
+        like = -(1 / 2) * \
+            np.matmul(self.training_labels.transpose(), self.alpha) - \
+            np.sum(np.log(np.diagonal(self.l_mat))) - np.log(2 * np.pi) * \
+            self.k_mat.shape[1] / 2
+
+        return like
+
+    def set_kernel(self, sigma_f, length_scale, sigma_n):
+        """
+        Compute 3Nx3N noiseless kernel matrix
+        """
+        d_s = ['xrel', 'yrel', 'zrel']
+
+        # initialize matrix
+        size = len(self.training_data) * 3
+        k_mat = np.zeros([size, size])
+
+        # calculate elements
+        for m_index in range(size):
+            x_1 = self.training_data[int(math.floor(m_index / 3))]
+            d_1 = d_s[m_index % 3]
+            for n_index in range(m_index, size):
+                x_2 = self.training_data[int(math.floor(n_index / 3))]
+                d_2 = d_s[n_index % 3]
+
+                # calculate kernel
+                cov = self.kernel(x_1, x_2, d_1, d_2, sigma_f, length_scale)
+                k_mat[m_index, n_index] = cov
+                k_mat[n_index, m_index] = cov
+
+        # perform cholesky decomposition and store
+        self.l_mat = np.linalg.cholesky(k_mat + sigma_n ** 2 * np.eye(size))
+        self.k_mat = k_mat
+
+    def get_kernel_vector(self, x, d_1):
+        """
+        Compute kernel vector
+        """
+        d_s = ['xrel', 'yrel', 'zrel']
+        size = len(self.training_data) * 3
+        k_v = np.zeros([size, 1])
+
+        for m in range(size):
+            x_2 = self.training_data[int(math.floor(m / 3))]
+            d_2 = d_s[m % 3]
+            k_v[m] = self.kernel(x, x_2, d_1, d_2,
+                                 self.sigma_f,
+                                 self.length_scale)
+
+        return k_v
+
+    def set_alpha(self):
+        """
+        Set weight vector alpha
+        """
+        ts1 = solve_triangular(self.l_mat, self.training_labels, lower=True)
+        alpha = solve_triangular(self.l_mat.transpose(), ts1)
+
+        self.alpha = alpha
+
+
+if __name__ == "__main__":
+
+    outfile = os.path.join(os.environ['ML_HOME'], '/data/SiC_MD/sic_md.out')
+    Si_MD_Parsed = parse_qe_pwscf_md_output(outfile)
+
+    # # set crystal structure
+    # dim = 3
+    # alat_si = 4.344404578
+    # unit_cell = [[0.0, alat_si / 2, alat_si / 2],
+    #              [alat_si / 2, 0.0, alat_si / 2],
+    #              [alat_si / 2, alat_si / 2, 0.0]]
+    # unit_pos = [['Si', [0, 0, 0]],
+    #             ['Si', [alat_si / 4, alat_si / 4, alat_si / 4]]]
+    # brav_mat_si = np.array([[0.0, alat_si / 2, alat_si / 2],
+    #                        [alat_si / 2, 0.0, alat_si / 2],
+    #                        [alat_si / 2, alat_si / 2, 0.0]]) * dim
+    # brav_inv_si = np.linalg.inv(brav_mat_si)
+    #
+    # # bravais vectors
+    # vec1_si = brav_mat_si[:, 0].reshape(3, 1)
+    # vec2_si = brav_mat_si[:, 1].reshape(3, 1)
+    # vec3_si = brav_mat_si[:, 2].reshape(3, 1)
+    # cutoff_si = 4.5
+    #
+    # positions, species, cell = parse_qe_input('../pwscf.in')
+    # forces = parse_qe_forces('../pwscf.out')
+    # structure = Structure(positions, species, cell)
+    #
+    # #
+    # pos = Si_MD_Parsed[1]['positions']
+    # fcs = fc_conv(Si_MD_Parsed[2]['forces'])
+    # #
+
+    # gp = GaussianProcess(kernel=two_body)
+    # gp.init_db(root_dir='..',
+    #            brav_mat=brav_mat_si,
+    #            brav_inv=brav_inv_si,
+    #            vec1=vec1_si, vec2=vec2_si, vec3=vec3_si,
+    #            cutoff=cutoff_si)
+    # gp.train()
+
---
+++ otf.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""""
+OTF engine
+
+Steven Torrisi, Jon Vandermause, Simon Batzner
+"""
+
+import numpy as np
+import os
+import datetime
+
+
+class Structure(object):
+    def __init__(self, positions, species, cell):
+        """
+        Contains positions, species, cell, previous positions, forces, and
+        stds of forces
+
+        :param positions: List[nparray],  List of np array positions
+        :param species: List[str], List of element symbols
+        :param cell: nparray, 3x3 np array of Bravais lattice
+        """
+        self.positions = positions
+        self.species = species
+        self.cell = cell
+
+        self.prev_positions = list(positions)
+        self.forces = [np.zeros(3) for pos in positions]
+        self.stds = [np.zeros(3) for pos in positions]
+
+
+class fake_kernel(object):
+    """
+    Fake kernel that returns random forces and variances. 
+    Hope we do better than this!
+    """
+
+    def train(self, structure):
+        # zippa dee doo da
+        pass
+
+    def predict(self, structure):
+        structure.forces = [np.random.randn(3) for n in structure.positions]
+        structure.stds = [np.random.randn(3) for n in structure.positions]
+
+        return structure
+
+
+mass_dict = {'H': 1.0,
+             'Si': 28.0855,
+             'C': 12.0107,
+             'Au': 196.96657,
+             'Ag': 107.8682,
+             'Pd': 106.42}
+
+
+class OTF(object):
+    def __init__(self, qe_input, dt, number_of_steps, kernel):
+        """
+        On-the-fly learning engine, containing methods to run OTF calculation
+
+        :param qe_input: str, Location of QE input
+        :param dt: float, Timestep size
+        :param number_of_steps: int, Number of steps
+        :param kernel: GP, Regression model object
+        """
+
+        self.qe_input = qe_input
+        self.dt = dt
+        self.Nsteps = number_of_steps
+        self.kernel = kernel
+
+        positions, species, cell = parse_qe_input(self.qe_input)
+        self.structure = Structure(positions, species, cell)
+
+        self.curr_step = 0
+
+        # Create blank output file with time header and structure information
+        with open('otf_run.out', 'w') as f:
+            f.write(str(datetime.datetime.now()) + '\n')
+            # f.write(str(self.structure.species)+'\n')
+
+    def run(self):
+        """
+        Performs main loop of OTF engine.
+        :return:
+        """
+
+        # Bootstrap first training point
+        self.structure.forces = self.run_espresso()
+        self.kernel.train(self.structure)
+
+        # Main loop
+        while self.curr_step < self.Nsteps:
+
+            self.structure = self.kernel.predict(self.structure)
+
+            if not self.is_std_in_bound():
+                self.structure.forces = self.run_espresso()
+                self.kernel.train(self.structure)
+                continue
+
+            self.update_positions()
+            self.write_step()
+            self.curr_step += 1
+
+    def update_positions(self):
+        """
+        Apply a timestep to the structure at self.structure based on current
+        forces.
+        """
+
+        # Maintain list of elemental masses in amu to calculate acceleration
+
+        for i, prev_pos in enumerate(self.structure.prev_positions):
+            temp_pos = self.structure.positions[i]
+            self.structure.positions[i] = 2 * self.structure.positions[i] - \
+                prev_pos + self.dt ** 2 * self.structure.forces[i] / \
+                mass_dict[self.structure.species[i]]
+            self.structure.prev_positions[i] = np.array(temp_pos)
+
+    def run_espresso(self):
+        """
+        Calls quantum espresso from input located at self.qe_input
+
+        :return: List [nparray] List of forces
+        """
+
+        self.write_qe_input_positions()
+
+        pw_loc = os.environ.get('PWSCF_COMMAND', 'pwscf.x')
+
+        qe_command = '{0} < {1} > {2}'.format(pw_loc, self.qe_input, 'pwscf.out')
+
+        os.system(qe_command)
+
+        return parse_qe_forces('pwscf.out')
+
+    def write_qe_input_positions(self):
+        """
+        Write the current configuration of the OTF structure to the qe input file
+        """
+
+        with open(self.qe_input, 'r') as f:
+            lines = f.readlines()
+
+        file_pos_index = None
+        for i, line in enumerate(lines):
+            if 'ATOMIC_POSITIONS' in line:
+                file_pos_index = int(i + 1)
+        assert file_pos_index is not None, 'Failed to find positions in input'
+
+        for pos_index, line_index in enumerate(range(file_pos_index, len(lines))):
+            pos_string = ' '.join([self.structure.species[pos_index],
+                                   str(self.structure.positions[pos_index][0]),
+                                   str(self.structure.positions[pos_index][1]),
+                                   str(self.structure.positions[pos_index][2])])
+            lines[line_index] = str(pos_string + '\n')
+
+        with open(self.qe_input, 'w') as f:
+            for line in lines:
+                f.write(line)
+
+    def write_step(self):
+        """
+        Write current step to the output file including positions, forces, and force variances
+        """
+
+        with open('otf_run.out', 'a') as f:
+
+            f.write("====================== \n")
+            f.write(
+                ' '.join(["-", "Frame", str(self.curr_step), 'Time', str(np.round(self.dt * self.curr_step, 3)), '\n']))
+
+            for i in range(len(self.structure.positions)):
+                to_write = [self.structure.species[i]]
+                for j in range(3):
+                    to_write.append(str(np.round(self.structure.positions[i][j], 3)))
+                for j in range(3):
+                    to_write.append(str(np.round(self.structure.forces[i][j], 3)))
+                for j in range(3):
+                    to_write.append(str(np.round(self.structure.stds[i][j], 3)))
+                curr_line = ','.join(to_write)
+                curr_line += '\n'
+
+                f.write(curr_line)
+
+    def is_std_in_bound(self):
+        """
+        Evaluate if the model error are within predefined criteria
+
+        :return: Bool, If model error is within acceptable bounds
+        """
+
+        # Some decision making criteria
+        return True
+
+
+def parse_qe_input(qe_input):
+    """
+    Reads the positions, species, and cell in from the
+
+    :param qe_input: str, Path to PWSCF input file
+    :return: List[nparray], List[str], nparray, Positions, species, 3x3 Bravais cell
+    """
+    positions = []
+    species = []
+    cell = []
+
+    with open(qe_input) as f:
+        lines = f.readlines()
+
+    # Find the cell and positions in the output file
+    cell_index = None
+    positions_index = None
+    for i, line in enumerate(lines):
+        if 'CELL_PARAMETERS' in line:
+            cell_index = int(i + 1)
+        if 'ATOMIC_POSITIONS' in line:
+            positions_index = int(i + 1)
+    assert cell_index is not None, 'Failed to find cell in output'
+    assert positions_index is not None, 'Failed to find positions in output'
+
+    # Load cell
+    for i in range(cell_index, cell_index + 3):
+        cell_line = lines[i].strip()
+        cell.append(np.fromstring(cell_line, sep=' '))
+    cell = np.array(cell)
+
+    # Check cell IO
+    assert cell != [], 'Cell failed to load'
+    assert np.shape(cell) == (3, 3), 'Cell failed to load correctly'
+
+    # Load positions
+    for i in range(positions_index, len(lines)):
+        line_string = lines[i].strip().split()
+        species.append(line_string[0])
+
+        pos_string = ' '.join(line_string[1:4])
+
+        positions.append(np.fromstring(pos_string, sep=' '))
+    # Check position IO
+    assert positions != [], "Positions failed to load"
+
+    return positions, species, cell
+
+
+def parse_qe_forces(outfile):
+    """
+    Get forces from a pwscf file in Ryd/bohr
+
+    :param outfile: str, Path to pwscf output file
+    :return: list[nparray] , List of forces acting on atoms
+    """
+    forces = []
+    total_energy = np.nan
+
+    with open(outfile, 'r') as outf:
+        for line in outf:
+            if line.lower().startswith('!    total energy'):
+                total_energy = float(line.split()[-2]) * 13.605698066
+
+            if line.find('force') != -1 and line.find('atom') != -1:
+                line = line.split('force =')[-1]
+                line = line.strip()
+                line = line.split(' ')
+                # print("Parsed line",line,"\n")
+                line = [x for x in line if x != '']
+                temp_forces = []
+                for x in line:
+                    temp_forces.append(float(x))
+                forces.append(list(temp_forces))
+
+    assert total_energy != np.nan, "Quantum ESPRESSO parser failed to read the file {}. Run failed.".format(outfile)
+
+    return forces
+
+
+def parse_output(outfile):
+    """
+    Parse the output of a otf run for analysis
+    :param outfile: str, Path to file
+    :return: dict{int:value,'species':list}, Dict of positions, forces, vars indexed by frame and of species
+    """
+
+    results = {}
+    with open(outfile, 'r') as f:
+        lines = f.readlines()
+
+    frame_indices = [lines.index(line) for line in lines if line[0] == '-']
+    n_atoms = frame_indices[1] - frame_indices[0] - 2
+
+    for frame_number, index in enumerate(frame_indices):
+        positions = []
+        forces = []
+        vars = []
+        species = []
+
+        for at_index in range(n_atoms):
+            at_line = lines[index + at_index + 1].strip().split(',')
+            species.append(at_line[0])
+            positions.append(np.fromstring(','.join((at_line[1], at_line[2], at_line[3])), sep=','))
+            forces.append(np.fromstring(','.join((at_line[4], at_line[5], at_line[6])), sep=','))
+            vars.append(np.fromstring(','.join((at_line[7], at_line[8], at_line[9])), sep=','))
+
+            results[frame_number] = {'positions': positions,
+                                     'forces': forces,
+                                     'vars': vars}
+
+        results['species'] = species
+        print(results)
+
+
+if __name__ == '__main__':
+    os.system('cp pwscf.in.orig pwscf.in')
+
+    k = fake_kernel()
+    otf = OTF('pwscf.in', .1, 10, kernel=k)
+    otf.run()
+    # parse_output('otf_run.out')
+    pass
