b9d1eb1ed7888b84545bc706113527f3d395afd6
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2018-08-31 10:33:46 -0400

    add bond_list method

--- src/struc.py
+++ src/struc.py
@@ -1,5 +1,6 @@
 from numpy import zeros
 from numpy.linalg import inv
+import numpy as np
 
 
 class Structure(object):
@@ -21,7 +22,7 @@ class Structure(object):
         self.vec3 = lattice[2, :]
 
         self.species = species
-        self.elements = set(species)
+        self.elements = list(set(species))
         self.bond_list = self.calc_bond_list(self.elements)
 
         self.inv_lattice = inv(lattice)
@@ -33,7 +34,36 @@ class Structure(object):
 
         self.cutoff = cutoff
 
-    def calc_bond_list(self, elements):
+    @staticmethod
+    def calc_bond_list(unique_species):
+        """Converts unique species to a list of bonds.
+
+        :param unique_species: unique species in the simulation
+        :type unique_species: list of strings
+        :return: all possible bonds
+        :rtype: list of lists of two strings
+        """
+
+        # create bond list
         bond_list = []
-        pass
+        for m in range(len(unique_species)):
+            species_1 = unique_species[m]
+
+            for n in range(m, len(unique_species)):
+                species_2 = unique_species[n]
+                bond_list.append([species_1, species_2])
+
         return bond_list
+
+if __name__ == '__main__':
+    # create simple structure
+    lattice = np.eye(3)
+    species = ['B', 'A']
+    positions = [np.array([0, 0, 0]), np.array([0.5, 0.5, 0.5])]
+    cutoff = np.linalg.norm(np.array([0.5, 0.5, 0.5])) + 0.001
+
+    test_structure = Structure(lattice, species, positions, cutoff)
+
+    # test species_to_bond
+    print(test_structure.bond_list)
+    assert(test_structure.bond_list == [['B', 'B'], ['B', 'A'], ['A', 'A']])
