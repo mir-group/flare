Added in correct units, and re-unitted unit tests accordingly

otf_engine/otf.py
@@ -10,7 +10,6 @@ Steven Torrisi, Jon Vandermause, Simon Batzner
 import numpy as np
 import datetime
 from struc import Structure
-from math import sqrt
 from gp import GaussianProcess
 from env import ChemicalEnvironment
 from punchout import punchout
@@ -84,7 +83,7 @@ class OTF(object):
             for i in range(3):
                 force, var = self.gp.predict(chemenv, i + 1)
                 self.structure.forces[n][i] = float(force)
-                self.structure.stds[n][i] = sqrt(var)
+                self.structure.stds[n][i] = np.sqrt(var)
 
     def run_and_train(self):
         """
@@ -113,10 +112,10 @@ class OTF(object):
         for n in range(self.train_structure.nat):
             qe_strings += self.train_structure.species[n] + ': '
             for i in range(3):
-                qe_strings += '%.3f  ' % self.train_structure.positions[n][i]
+                qe_strings += '%.8f  ' % self.train_structure.positions[n][i]
             qe_strings += '\t '
             for i in range(3):
-                qe_strings += '%.3f  ' % forces[n][i]
+                qe_strings += '%.8f  ' % forces[n][i]
             qe_strings += '\n'
         self.write_to_output(qe_strings)
 
@@ -125,11 +124,11 @@ class OTF(object):
         self.gp.update_db(self.train_structure, forces)
         self.gp.train()
         self.write_to_output('New GP Hyperparameters:\n' +
-                             'Signal variance: \t' +
+                             'Signal std: \t' +
                              str(self.gp.sigma_f) + '\n' +
                              'Length scale: \t\t' +
                              str(self.gp.length_scale) + '\n' +
-                             'Noise variance: \t' + str(self.gp.sigma_n) +
+                             'Noise std: \t' + str(self.gp.sigma_n) +
                              '\n'
                              )
 
@@ -140,6 +139,7 @@ class OTF(object):
 
         # Precompute dt squared for efficiency
         dtdt = self.dt ** 2
+
     
         for i, pre_pos in enumerate(self.structure.prev_positions):
             temp_pos = np.array(self.structure.positions[i])
@@ -184,7 +184,7 @@ class OTF(object):
         for i in range(len(self.structure.positions)):
             string += self.structure.species[i] + ' '
             for j in range(3):
-                string += str("%.6f" % self.structure.positions[i][j]) + ' '
+                string += str("%f"%self.structure.positions[i][j]) + ' '
             string += '\t'
             for j in range(3):
                 string += str(np.round(self.structure.forces[i][j], 6)) + ' '
@@ -263,7 +263,9 @@ def parse_otf_output(outfile: str):
 
 
 if __name__ == '__main__':
-    # os.system('cp ../tests/test_files/qe_input_2.in pwscf.in')
+
+    import os
+    os.system('cp qe_input_1.in pwscf.in')
 
     otf = OTF('pwscf.in', .1, 10, kernel='two_body',
               cutoff=10, punchout_d=None)

tests/test_OTF.py
@@ -85,6 +85,8 @@ def cleanup_otf_run():
     os.system('rm pwscf.wfc')
     os.system('rm pwscf.save')
     os.system('rm otf_run.out')
+    os.system('rm pwscf.in')
+    os.system('rm pwscf.wfc1')
 
 
 # ------------------------------------------------------
@@ -161,9 +163,9 @@ def test_cell_parsing(qe_input, exp_cell):
 @pytest.mark.parametrize("qe_input,mass_dict",
                          [
                              ('./test_files/qe_input_1.in',
-                              {'H': 1.0}),
+                              {'H': 9648.53}),
                              ('./test_files/qe_input_2.in',
-                              {'Al': 26.9815385})
+                              {'Al': 9648.53*26.9815385})
                          ]
                          )
 def test_cell_parsing(qe_input, mass_dict):
@@ -174,8 +176,8 @@ def test_cell_parsing(qe_input, mass_dict):
 @pytest.mark.parametrize('qe_output,exp_forces',
                          [
                              ('./test_files/qe_output_1.out',
-                              [np.array([0.07413986, 0., 0.]),
-                               np.array([-0.07413986, 0., 0.])])
+                              [np.array([1.9062131355, 0., 0.]),
+                               np.array([-1.9062131355, 0., 0.])])
                          ]
                          )
 def test_force_parsing(qe_output, exp_forces):
@@ -183,7 +185,7 @@ def test_force_parsing(qe_output, exp_forces):
     assert len(forces) == len(exp_forces)
 
     for i, force in enumerate(forces):
-        assert np.all(force == exp_forces[i])
+        assert np.isclose(force, exp_forces[i]).all()
 
 
 def test_espresso_calling_1(test_otf_engine_1):
@@ -197,11 +199,11 @@ def test_espresso_calling_1(test_otf_engine_1):
     assert len(forces) == len(test_otf_engine_1.structure.forces)
 
     if test_otf_engine_1.qe_input == './test_files/qe_input_1.in':
-        test1forces = [np.array([0.07413986, 0.0, 0.0]),
-                       np.array([-0.07413986,
+        test1forces = [np.array([1.90621314, 0.0, 0.0]),
+                       np.array([-1.90621314,
                                  0.0, 0.0])]
         for i, force in enumerate(forces):
-            assert np.equal(force, test1forces[i]).all()
+            assert np.isclose(force, test1forces[i]).all()
 
     cleanup_otf_run()
 
@@ -234,7 +236,6 @@ def test_espresso_input_edit():
 # ------------------------------------------------------
 
 # TODO see if there is a better way to to set up the different input runs
-
 def test_update_1(test_otf_engine_1):
     test_otf_engine_1.structure.prev_positions = [[2.5, 2.5, 2.5],
                                                   [4.5, 2.5, 2.5]]
@@ -244,8 +245,8 @@ def test_update_1(test_otf_engine_1):
 
     test_otf_engine_1.update_positions()
 
-    target_positions = [np.array([2.53714741, 2.5, 2.5]),
-                        np.array([4.46285259, 2.5, 2.5])
+    target_positions = [np.array([2.53714, 2.5, 2.5]),
+                        np.array([4.46286, 2.5, 2.5])
                         ]
 
     for i, pos in enumerate(test_otf_engine_1.structure.positions):