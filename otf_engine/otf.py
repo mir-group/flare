Fixed factor of a million error

otf_engine/otf.py
@@ -183,10 +183,10 @@ class OTF(object):
         for i in range(len(self.structure.positions)):
             string += self.structure.species[i] + ' '
             for j in range(3):
-                string += str("%f"%self.structure.positions[i][j]) + ' '
+                string += str("%.8f"%self.structure.positions[i][j]) + ' '
             string += '\t'
             for j in range(3):
-                string += str(np.round(self.structure.forces[i][j], 6)) + ' '
+                string += str(np.round(self.structure.forces[i][j], 8)) + ' '
             string += '\t'
             for j in range(3):
                 string += str('%.6e' % self.structure.stds[i][j]) + ' '
@@ -266,7 +266,7 @@ if __name__ == '__main__':
     import os
     os.system('cp qe_input_1.in pwscf.in')
 
-    otf = OTF('pwscf.in', .1, 10, kernel='two_body',
+    otf = OTF('pwscf.in', .001, 10, kernel='two_body',
               cutoff=10, punchout_d=None)
     otf.run()
     # parse_output('otf_run.out')

tests/test_OTF.py
@@ -163,9 +163,9 @@ def test_cell_parsing(qe_input, exp_cell):
 @pytest.mark.parametrize("qe_input,mass_dict",
                          [
                              ('./test_files/qe_input_1.in',
-                              {'H': 9648.53}),
+                              {'H': 0.00010364269933008285}),
                              ('./test_files/qe_input_2.in',
-                              {'Al': 9648.53*26.9815385})
+                              {'Al': 0.00010364269933008285*26.9815385})
                          ]
                          )
 def test_cell_parsing(qe_input, mass_dict):
@@ -245,8 +245,8 @@ def test_update_1(test_otf_engine_1):
 
     test_otf_engine_1.update_positions()
 
-    target_positions = [np.array([2.53714, 2.5, 2.5]),
-                        np.array([4.46286, 2.5, 2.5])
+    target_positions = [np.array([2.60867409, 2.5, 2.5]),
+                        np.array([4.39132591, 2.5, 2.5])
                         ]
 
     for i, pos in enumerate(test_otf_engine_1.structure.positions):