5b94949f053d878af806a09c8ec98a0527d12c9c
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2018-09-24 18:51:34 -0400

    add radial integrals

--- otf_engine/soap.py
+++ otf_engine/soap.py
@@ -2,7 +2,10 @@ from scipy.special import sph_harm
 from scipy.special import spherical_in
 from scipy.special import erf
 from scipy.special import gamma
-import matplotlib.pyplot as plt
+from scipy.integrate import quad
+from scipy.linalg import cholesky
+from numpy.linalg import inv
+# import matplotlib.pyplot as plt
 import numpy as np
 import math
 
@@ -11,55 +14,77 @@ import math
 # basis functions: gaussians of variable sigma centered at the origin
 # -------------------------------------------------------------------
 
-def get_basis(rs, l, n, n_tot, sigma_min, sigma_max):
-    sigma = sigma_min + ((sigma_max - sigma_min)/(n_tot - 1)) * n
-    beta = 1 / (2 * sigma**2)
-    basis_vals = rs**l * np.exp((-beta * rs**2))
+def get_betas(n_tot, sigma_min, sigma_max):
+    step = (sigma_max - sigma_min)/(n_tot - 1)
+    betas = np.zeros([n_tot])
+
+    for n in range(n_tot):
+        sigma = sigma_min + step * n
+        beta = 1 / (2 * sigma**2)
+        betas[n] = beta
+    return betas
+
+
+def get_basis(rs, beta, l):
+    basis_vals = rs**l * np.exp(-beta * rs**2)
     return basis_vals
 
 
-def plot_basis(rs, l, n, n_tot, sigma_min, sigma_max):
-    basis_vals = get_basis(rs, l, n, n_tot, sigma_min, sigma_max)
-    plt.plot(rs, basis_vals)
-
-
-def gauss_overlap(n, nprime, l, n_tot, sigma_min, sigma_max):
-    step = (sigma_max - sigma_min)/(n_tot - 1)
-    sigma = sigma_min + step * n
-    sigma_prime = sigma_min + step * nprime
-
-    beta = 1 / (2 * sigma**2)
-    beta_prime = 1 / (2 * sigma_prime**2)
-
+def gauss_overlap(beta, beta_prime, l):
     overlap = (1/2) * (beta + beta_prime)**(-(3/2)-l) * gamma((3/2)+l)
-
     return overlap
 
 
-def gauss_overlap_matrix(l, n_tot, sigma_min, sigma_max):
-    overlap = np.zeros([n_tot, n_tot])
-    for m in range(n_tot):
-        for n in range(n_tot):
-            overlap[m, n] = gauss_overlap(m, n, l, n_tot, sigma_min, sigma_max)
-    return overlap
+def gauss_overlap_matrix(l, betas):
+    overlap_matrix = np.zeros([len(betas), len(betas)])
+    for m in range(len(betas)):
+        beta = betas[m]
+        for n in range(m, len(betas)):
+            beta_prime = betas[n]
+            overlap = gauss_overlap(beta, beta_prime, l)
+            overlap_matrix[m, n] = overlap
+            overlap_matrix[n, m] = overlap
+    chol_mat = cholesky(overlap_matrix, lower=True)
+    basis_mat = inv(chol_mat)
+    return basis_mat
 
 
-def plot_gauss_basis(rs, l, n, n_tot, sigma_min, sigma_max):
-    comb_mat = gauss_overlap_matrix(l, n_tot, sigma_min, sigma_max)
-    ortho_vals = np.zeros(len(rs))
-    for m in range(n_tot):
-        ortho_vals += comb_mat[n, m] * get_basis(rs, l, m, n_tot, sigma_min,
-                                                 sigma_max)
-    plt.plot(rs, ortho_vals)
-
-
-def return_ortho_val(r, l, n, n_tot, sigma_min, sigma_max):
-    comb_mat = gauss_overlap_matrix(l, n_tot, sigma_min, sigma_max)
+def return_ortho_val(r, n, l, betas, basis_mat):
     ortho_val = 0
-    for m in range(n_tot):
-        ortho_val += comb_mat[n, m] * get_basis(r, l, m, n_tot, sigma_min, 
-                                                sigma_max)
+    for m in range(len(betas)):
+        beta = betas[m]
+        ortho_val += basis_mat[n, m] * \
+            get_basis(r, beta, l)
     return ortho_val
+
+
+# -------------------------------------------------------------------
+#                   compute radial integrals
+# -------------------------------------------------------------------
+
+def get_orth_ints(ri, l, alpha, betas, basis_mat):
+    non_orth_ints = np.sqrt(np.pi / (16 * ri * alpha)) * \
+        (((ri * alpha)**(l + (1/2))) / ((alpha + betas)**(l + (3/2)))) *\
+        np.exp(-(alpha * betas * ri**2)/(alpha + betas))
+
+    orth_ints = np.matmul(basis_mat, non_orth_ints)
+
+    return orth_ints
+
+
+def exact_radial(r, ri, alpha):
+    return np.exp(-alpha * (r**2 + ri**2)) * spherical_in(l, 2*alpha*r*ri)
+
+
+def reconstruct_radial(r, ri, l, betas, basis_mat, orth_ints):
+    ortho_vals = np.zeros([len(betas)])
+    for n in range(len(betas)):
+        ortho_vals[n] = return_ortho_val(r, n, l, betas, basis_mat)
+
+    radial_est = np.sum(orth_ints * ortho_vals)
+
+    return radial_est
+
 # -------------------------------------------------------------------
 #                some preliminary formula checks
 # -------------------------------------------------------------------
@@ -148,11 +173,71 @@ if __name__ == '__main__':
     max_l = 14
 
     rho_est = estimate_rho(pos, atom_pos_list, alpha, max_l)
-    print(rho_est)
-
     rho_ex = rho_exact(pos, atom_pos_list, alpha)
-    print(rho_ex)
 
-    # test basis_overlap
-    test_overlap = basis_overlap(4, 6, 14, 5, 0.5)
-    print(test_overlap)
+    assert(np.isclose(rho_est, rho_ex))
+
+    # test get_betas
+    n_tot = 5
+    sigma_min = 0.1
+    sigma_max = 5
+    betas = get_betas(n_tot, sigma_min, sigma_max)
+    assert(len(betas) == n_tot)
+    assert(betas[0] == 1 / (2 * sigma_min**2))
+    assert(betas[n_tot-1] == 1 / (2 * sigma_max**2))
+
+    # test gauss_overlap_matrix and return_ortho_val
+    # basis functions should be orthonormal
+    n_tot = 5
+    sigma_min = 0.1
+    sigma_max = 5
+    l = 0
+    n = 4
+    nprime = 3
+    betas = get_betas(n_tot, sigma_min, sigma_max)
+    basis_mat = gauss_overlap_matrix(l, betas)
+
+    zer_test = quad(lambda x: x**2 *
+                    return_ortho_val(x, n, l, betas, basis_mat) *
+                    return_ortho_val(x, nprime, l, betas, basis_mat),
+                    0, 30)[0]
+
+    one_test = quad(lambda x: x**2 *
+                    return_ortho_val(x, n, l, betas, basis_mat) *
+                    return_ortho_val(x, n, l, betas, basis_mat),
+                    0, 30)[0]
+
+    assert(np.isclose(zer_test, 0))
+    assert(np.isclose(one_test, 1))
+
+    # test get_orth_ints
+    # analytical and numerical calculations should agree
+    ri = 2
+    l = 0
+    alpha = 0.4
+
+    orth_ints = get_orth_ints(ri, l, alpha, betas, basis_mat)
+
+    n = 2
+    int_test = quad(lambda x: x**2 *
+                    return_ortho_val(x, n, l, betas, basis_mat) *
+                    exact_radial(x, ri, alpha),
+                    0, 30)[0]
+    assert(np.isclose(orth_ints[n], int_test))
+
+    # test reconstruct_radial
+    n_tot = 20
+    sigma_min = 0.05
+    sigma_max = 4
+    l = 0
+    ri = 4
+    alpha = 0.4
+    r = 4
+
+    betas = get_betas(n_tot, sigma_min, sigma_max)
+    basis_mat = gauss_overlap_matrix(l, betas)
+    orth_ints = get_orth_ints(ri, l, alpha, betas, basis_mat)
+
+    exact_val = exact_radial(r, ri, alpha)
+    est_val = reconstruct_radial(r, ri, l, betas, basis_mat, orth_ints)
+    assert(np.isclose(exact_val, est_val, 1e-4))
