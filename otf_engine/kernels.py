afe35ccb649f6b9d42897f10b996ff58e3277186
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2018-10-27 15:55:17 -0400

    add force/energy kernel

--- otf_engine/kernels.py
+++ otf_engine/kernels.py
@@ -206,12 +206,45 @@ def two_body_grad_from_env(bond_array_1, bond_types_1, bond_array_2,
 # -----------------------------------------------------------------------------
 
 
+# single component n body energy/force kernel
+def energy_force_sc(bond_array_1, cross_bond_dists_1, combinations,
+                    bond_array_2, cross_bond_dists_2, permutations,
+                    d1, d2, hyps):
+    sig = hyps[0]
+    ls = hyps[1]
+    kern = 0
+
+    for m in range(combinations.shape[0]):
+        comb = combinations[m]
+        for n in range(permutations.shape[0]):
+            perm = permutations[n]
+            B_cp_1 = 0
+            C_cp = 0
+
+            for q, (c_ind, p_ind) in enumerate(zip(comb, perm)):
+                rdiff = bond_array_1[c_ind, 0] - bond_array_2[p_ind, 0]
+                coord1 = bond_array_1[c_ind, d1]
+                coord2 = bond_array_2[p_ind, d2]
+
+                B_cp_1 += -rdiff * coord1
+                C_cp += rdiff * rdiff
+
+                for c_ind_2, p_ind_2 in zip(comb[q+1:], perm[q+1:]):
+                    cb_diff = cross_bond_dists_1[c_ind, c_ind_2] - \
+                        cross_bond_dists_2[p_ind, p_ind_2]
+                    C_cp += cb_diff * cb_diff
+
+            kern += (sig*sig / (ls*ls)) * B_cp_1 * \
+                exp(-C_cp / (2 * ls * ls))
+
+    return kern
+
+
 # single component n body kernel
 @njit
 def n_body_jit_sc(bond_array_1, cross_bond_dists_1, combinations,
                   bond_array_2, cross_bond_dists_2, permutations,
                   d1, d2, hyps):
-
     sig = hyps[0]
     ls = hyps[1]
     kern = 0
