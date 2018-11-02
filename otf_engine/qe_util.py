fa2a03dc54e1a91e0399c2f951c360f3a72dd2d7
Steven T <torrisi@g.harvard.edu>
2018-11-02 16:00:18 -0400

    Added more detail to units

--- otf_engine/qe_util.py
+++ otf_engine/qe_util.py
@@ -156,7 +156,7 @@ def parse_qe_input(qe_input: str) -> (
     assert positions != [], "Positions failed to load"
 
     # Load masses
-    # Convert from amu to mass units with picosecond timescale and angstrom
+    # Convert from amu to  mass units with picosecond timescale and angstrom
     # length scale such that the unit of energy is in natural units (=1)
     massconvert = 0.00010364269933008285
     masses = {}
