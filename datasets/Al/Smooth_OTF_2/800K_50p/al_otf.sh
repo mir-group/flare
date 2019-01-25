7447de60b101346d530c2e8c0437ce01a551fea4
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-01-25 18:00:41 -0500

    add al otf run

---
+++ datasets/Al/Smooth_OTF_2/800K_50p/al_otf.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH -n 32
+#SBATCH -N 1
+#SBATCH -t 14-00:00
+#SBATCH -e test.err
+#SBATCH -p kozinsky
+#SBATCH -o test.out
+#SBATCH --mem-per-cpu=5000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jonathan_vandermause@g.harvard.edu
+
+module load gcc/4.9.3-fasrc01 openmpi/2.1.0-fasrc01
+module load python/3.6.3-fasrc01
+
+python al_otf.py
--- datasets/Al_vac/pc_np_Al_vac/README.txt
+++
@@ -1,4 +0,0 @@
-31 atoms
-lattice param found from QE vc-relax
-no initial perturbations, no NVT setup
-
