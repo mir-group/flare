dd3e791a918a03250f4f94f7d0203a7d34d1695a
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-01-28 14:56:30 -0500

    update otf parser

--- modules/otf_parser.py
+++ modules/otf_parser.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import crystals
+from typing import List, Tuple
 sys.path.append('../otf_engine')
 import gp, env, struc, kernels, otf
 
@@ -9,21 +10,28 @@ class OtfAnalysis:
     def __init__(self, filename):
         self.filename = filename
 
-        positions, dft_frames, temperatures, times, msds, dft_times = \
+        position_list, force_list, uncertainty_list, velocity_list,\
+            dft_frames, temperatures, times, msds, dft_times = \
             self.parse_pos_otf(filename)
 
-        self.positions = positions
+        self.position_list = position_list
+        self.force_list = force_list
+        self.uncertainty_list = uncertainty_list
+        self.velocity_list = velocity_list
         self.dft_frames = dft_frames
         self.temperatures = temperatures
         self.times = times
         self.msds = msds
         self.dft_times = dft_times
 
-        gp_pos_list, gp_force_list, gp_atom_list, gp_hyp_list, \
-            gp_cutoff_radius, gp_species_list = self.extract_gp_info(filename)
+        gp_position_list, gp_force_list, gp_uncertainty_list,\
+            gp_velocity_list, gp_atom_list, gp_hyp_list, gp_cutoff_radius,\
+            gp_species_list = self.extract_gp_info(filename)
 
-        self.gp_pos_list = gp_pos_list
+        self.gp_position_list = gp_position_list
         self.gp_force_list = gp_force_list
+        self.gp_uncertainty_list = gp_uncertainty_list
+        self.gp_velocity_list = gp_velocity_list
         self.gp_atom_list = gp_atom_list
         self.gp_hyp_list = gp_hyp_list
         self.gp_cutoff_radius = gp_cutoff_radius
@@ -35,7 +43,7 @@ class OtfAnalysis:
         gp_model.hyps = gp_hyps
 
         for count, (positions, forces, atom, _, species) in \
-            enumerate(zip(self.gp_pos_list, self.gp_force_list,
+            enumerate(zip(self.gp_position_list, self.gp_force_list,
                           self.gp_atom_list, self.gp_hyp_list,
                           self.gp_species_list)):
             if count < call_no:
@@ -53,7 +61,10 @@ class OtfAnalysis:
         pass
 
     def parse_pos_otf(self, filename):
-        positions = []
+        position_list = []
+        force_list = []
+        uncertainty_list = []
+        velocity_list = []
         temperatures = []
         dft_frames = []
         dft_times = []
@@ -77,61 +88,60 @@ class OtfAnalysis:
 
             if line.startswith("- Frame"):
                 n_steps += 1
-                pos = []
-
                 frame_line = line.split()
                 sim_time = float(frame_line[5])
                 times.append(sim_time)
 
-                for frame_line in lines[(index+2):(index+2+noa)]:
-                    frame_line = frame_line.split()
-                    curr_pos = np.zeros(shape=(3,))
-                    curr_pos[0] = str(frame_line[1])
-                    curr_pos[1] = str(frame_line[2])
-                    curr_pos[2] = str(frame_line[3])
+                _, positions, forces, uncertainties, velocities = \
+                    parse_snapshot(lines, index, noa, False)
 
-                    pos.append(curr_pos)
+                position_list.append(positions)
+                force_list.append(forces)
+                uncertainty_list.append(uncertainties)
+                velocity_list.append(velocities)
 
                 temp_line = lines[index+2+noa].split()
                 temperatures.append(float(temp_line[1]))
 
-                positions.append(np.array(pos))
+                msds.append(np.mean((positions - position_list[0])**2))
 
-                msds.append(np.mean((pos - positions[0])**2))
-
-        return positions, dft_frames, temperatures, times, msds, dft_times
+        return position_list, force_list, uncertainty_list, velocity_list,\
+            dft_frames, temperatures, times, msds, dft_times
 
     def extract_gp_info(self, filename):
-        pos_list = []
         species_list = []
-        atom_list = []
+        position_list = []
         force_list = []
+        uncertainty_list = []
+        velocity_list = []
+        atom_list = []
         hyp_list = []
 
         with open(filename, 'r') as f:
             lines = f.readlines()
 
         for index, line in enumerate(lines):
+            # cutoff radius
             if line.startswith("Structure Cutoff Radius:"):
                 line_curr = line.split()
                 cutoff_radius = float(line_curr[3])
 
+            # number of atoms
             if line.startswith("Number of Atoms"):
                 line_curr = line.split()
                 noa = int(line_curr[3])
 
+            # number of hyperparameters
             if line.startswith("# Hyperparameters"):
                 line_curr = line.split()
                 noh = int(line_curr[2])
 
+            # TODO: change otf program so that this if statement is unnecessary
             if line.startswith("Calling DFT with training atoms"):
                 line_curr = line.split()
 
-                pos = []
-                fcs = []
+                # TODO: write function for updating hyps list
                 hyps = []
-                species = []
-
                 for frame_line in lines[(index+4):(index+4+noh)]:
                     frame_line = frame_line.split()
                     hyps.append(float(frame_line[5]))
@@ -139,32 +149,13 @@ class OtfAnalysis:
                 hyp_list.append(hyps)
                 hyp_list.append(hyps)
 
-                for frame_line in lines[(index+12):(index+12+noa)]:
-                    frame_line = frame_line.split()
-                    curr_pos = np.zeros(shape=(3,))
-                    curr_pos[0] = str(frame_line[1])
-                    curr_pos[1] = str(frame_line[2])
-                    curr_pos[2] = str(frame_line[3])
-
-                    dft_fc = np.zeros(shape=(3,))
-                    dft_fc[0] = str(frame_line[4])
-                    dft_fc[1] = str(frame_line[5])
-                    dft_fc[2] = str(frame_line[6])
-
-                    species.append(str(frame_line[0]))
-                    pos.append(curr_pos)
-                    fcs.append(dft_fc)
-
-                pos = np.array(pos)
-                pos_list.append(pos)
-                pos_list.append(pos)
-
-                species_list.append(species)
-                species_list.append(species)
-
-                fcs = np.array(fcs)
-                force_list.append(fcs)
-                force_list.append(fcs)
+                # TODO: generalize this to account for arbitrary starting list
+                append_atom_lists(species_list, position_list, force_list,
+                                  uncertainty_list, velocity_list,
+                                  lines, index, noa, True)
+                append_atom_lists(species_list, position_list, force_list,
+                                  uncertainty_list, velocity_list,
+                                  lines, index, noa, True)
 
                 atom_list.append(0)
                 atom_list.append(30)
@@ -172,47 +163,104 @@ class OtfAnalysis:
             if line.startswith("Calling DFT due to"):
                 line_curr = line.split()
 
-                pos = []
-                fcs = []
+                # TODO: write function for updating hyps list
                 hyps = []
-                species = []
-
                 for frame_line in lines[(index+4):(index+4+noh)]:
                     frame_line = frame_line.split()
                     hyps.append(float(frame_line[5]))
                 hyps = np.array(hyps)
                 hyp_list.append(hyps)
 
-                for frame_line in lines[(index+12):(index+12+noa)]:
-                    frame_line = frame_line.split()
-                    curr_pos = np.zeros(shape=(3,))
-                    curr_pos[0] = str(frame_line[1])
-                    curr_pos[1] = str(frame_line[2])
-                    curr_pos[2] = str(frame_line[3])
-
-                    dft_fc = np.zeros(shape=(3,))
-                    dft_fc[0] = str(frame_line[4])
-                    dft_fc[1] = str(frame_line[5])
-                    dft_fc[2] = str(frame_line[6])
-
-                    pos.append(curr_pos)
-                    fcs.append(dft_fc)
-
-                    species.append(str(frame_line[0]))
-
-                pos = np.array(pos)
-                pos_list.append(pos)
-
-                fcs = np.array(fcs)
-                force_list.append(fcs)
-
-                species_list.append(species)
-
+                # TODO: generalize atom list update to account for arbitrary
+                # list
+                append_atom_lists(species_list, position_list, force_list,
+                                  uncertainty_list, velocity_list,
+                                  lines, index, noa, True)
                 atom_list.append(int(line_curr[5]))
 
-        return pos_list, force_list, atom_list, hyp_list, cutoff_radius,\
+        return position_list, force_list, uncertainty_list, velocity_list,\
+            atom_list, hyp_list, cutoff_radius,\
             species_list
 
 
-def get_gp_from_file(filename, cell):
-    pass
+def append_atom_lists(species_list: List[str],
+                      position_list: List[np.ndarray],
+                      force_list: List[np.ndarray],
+                      uncertainty_list: List[np.ndarray],
+                      velocity_list: List[np.ndarray],
+                      lines: List[str], index: int, noa: int,
+                      dft_call: bool) -> None:
+
+    """Update lists containing atom information at each snapshot."""
+
+    species, positions, forces, uncertainties, velocities = \
+        parse_snapshot(lines, index, noa, dft_call)
+
+    species_list.append(species)
+    position_list.append(positions)
+    force_list.append(forces)
+    uncertainty_list.append(uncertainties)
+    velocity_list.append(velocities)
+
+
+def parse_snapshot(lines, index, noa, dft_call):
+    """Parses snapshot of otf output file.
+
+    :param lines: list of lines in output file
+    :type lines: list
+    :param index: index of snapshot
+    :type index: int
+    :param noa: number of atoms in simulation
+    :type noa: int
+    :param dft_call: whether or not dft was called for this snapshot
+    :type dft_call: bool
+    :return: species, positions, forces, uncertainties, and velocities
+    :rtype: list, np.arrays
+    """
+
+    # initialize values
+    species = []
+    positions = np.zeros((noa, 3))
+    forces = np.zeros((noa, 3))
+    uncertainties = np.zeros((noa, 3))
+    velocities = np.zeros((noa, 3))
+
+    # number of lines to skip after frame line
+    if dft_call is True:
+        skip = 12
+    else:
+        skip = 2
+
+    for count, frame_line in enumerate(lines[(index+skip):(index+skip+noa)]):
+        # parse frame line
+        spec, position, force, uncertainty, velocity = \
+            parse_frame_line(frame_line)
+
+        # update values
+        species.append(spec)
+        positions[count] = position
+        forces[count] = force
+        uncertainties[count] = uncertainty
+        velocities[count] = velocity
+
+    return species, positions, forces, uncertainties, velocities
+
+
+def parse_frame_line(frame_line):
+    """parse a line in otf output.
+
+    :param frame_line: frame line to be parsed
+    :type frame_line: string
+    :return: species, position, force, uncertainty, and velocity of atom
+    :rtype: list, np.arrays
+    """
+
+    frame_line = frame_line.split()
+
+    spec = str(frame_line[0])
+    position = np.array([float(n) for n in frame_line[1:4]])
+    force = np.array([float(n) for n in frame_line[4:7]])
+    uncertainty = np.array([float(n) for n in frame_line[7:10]])
+    velocity = np.array([float(n) for n in frame_line[10:13]])
+
+    return spec, position, force, uncertainty, velocity
