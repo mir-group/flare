fix likelihood assigned to non positive definite covariance matrix

modules/otf_parser.py
@@ -199,6 +199,7 @@ class OtfAnalysis:
                     # keep track of atom number
                     if line_check.startswith("Adding atom"):
                         line_split = line_check.split()
+                        print(line_split)
                         atoms_added.append(int(line_split[2]))
 
                     # keep track of hyperparameters
@@ -209,7 +210,6 @@ class OtfAnalysis:
                             hyps.append(float(hyp_line[-1]))
                         hyps = np.array(hyps)
                         hyps_added.append(hyps)
-
 
                     ind_count += 1
                     line_check = lines[ind_count]

otf_engine/gp_algebra.py
@@ -240,7 +240,7 @@ def get_like_grad_from_mats(ky_mat, hyp_mat, training_labels_np):
             ky_mat_inv = np.linalg.inv(ky_mat)
             l_mat = np.linalg.cholesky(ky_mat)
         except:
-            return 1e8, np.zeros(number_of_hyps)
+            return -1e8, np.zeros(number_of_hyps)
 
         alpha = np.matmul(ky_mat_inv, training_labels_np)
         alpha_mat = np.matmul(alpha.reshape(alpha.shape[0], 1),