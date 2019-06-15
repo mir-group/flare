3cba0c86bb4190db843da76bf7dfad627b97c8b7
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-06-15 12:36:11 -0400

    implement random velocities for multicomponent systems

--- flare/modules/initialize_velocities.py
+++ flare/modules/initialize_velocities.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def get_random_velocities(noa: int, temperature: float, mass: float):
     """Draw velocities from Maxwell-Boltzmann distribution. Assumes mass
     is given in amu."""
@@ -13,3 +14,27 @@ def get_random_velocities(noa: int, temperature: float, mass: float):
     corrected_velocities = velocities - vel_sum / noa
 
     return corrected_velocities
+
+
+# TODO: implement velocity function for multicomponent systems
+def multicomponent_velocities(temperature, masses):
+    noa = len(masses)
+    velocities = np.zeros((noa, 3))
+    kb = 0.0000861733034
+    mom_tot = np.array([0., 0., 0.])
+
+    for n, mass in enumerate(masses):
+        mass_md = mass * 0.000103642695727
+        std = np.sqrt(kb * temperature / mass_md)
+        rand_vel = np.random.normal(scale=std, size=(3))
+        velocities[n] = rand_vel
+        mom_curr = rand_vel * mass_md
+        mom_tot += mom_curr
+
+    # correct momentum
+    mom_corr = mom_tot / noa
+    for n, mass in enumerate(masses):
+        mass_md = mass * 0.000103642695727
+        velocities[n] -= mom_corr / mass_md
+
+    return velocities
