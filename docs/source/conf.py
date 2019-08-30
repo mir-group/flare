dc004ea2c265be20e70cd2f3b1fe071a6c0e5e4d
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-08-30 13:50:28 -0400

    add flare to conf.py path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-# sys.path.append('/Users/jonpvandermause/Research/GP/flare/')
+sys.path.append('../../flare')
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
