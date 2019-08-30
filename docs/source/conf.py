7bf8b90fccbccd9ff2c59afbd389cec8950cea43
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-08-30 13:53:12 -0400

    fix path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.append('../../flare')
+sys.path.append('../..')
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
