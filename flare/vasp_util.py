818cc1dd5ae83aabddd57603c4cc9bc1ebd95428
Steven T <torrisi@g.harvard.edu>
2019-09-05 14:23:34 -0400

    Adding vasp util (in development)

--- flare/vasp_util.py
+++ flare/vasp_util.py
@@ -6,5 +6,12 @@ from flare.struc import Structure
 from ase.io import read
 
 
-def vasprun_to_structures():
+def vasprun_to_structures(filename,):
+    """
+    Loop through Vasprun.xml object and convert all
+    frames into FLARE structures.
+    :return:
+    """
 
+    raise NotImplementedError
+    #vasprun_structures =
