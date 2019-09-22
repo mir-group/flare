9c20ad4a274708a683ae8e887c58ac6f3b9d530d
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-09-22 12:43:19 -0400

    add header to env

--- flare/struc.py
+++ flare/struc.py
@@ -1,8 +1,4 @@
-"""The :class:`Structure` object is a collection of atoms in a periodic \
-box. The mandatory inputs are the cell vectors of the box and the chemical species and Cartesian coordinates of the atoms. The atoms are automatically \
-folded back into the primary cell, so the input coordinates don't need to lie \
-inside the box. Energy, force, and stress information can be provided \
-for training machine learned force fields."""
+"""The :class:`Structure` object is a collection of atoms in a periodic box. The mandatory inputs are the cell vectors of the box and the chemical species and Cartesian coordinates of the atoms. The atoms are automatically folded back into the primary cell, so the input coordinates don't need to lie inside the box. Energy, force, and stress information can be provided for training machine learned force fields."""
 import numpy as np
 from typing import List
 from flare.util import element_to_Z, NumpyEncoder
