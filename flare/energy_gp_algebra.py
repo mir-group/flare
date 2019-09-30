f3941dc51e1965a36b6b7ca7d642b9d1fc3ddcdf
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-09-29 22:06:49 -0400

    implement ky block

--- flare/energy_gp_algebra.py
+++ flare/energy_gp_algebra.py
@@ -1,9 +1,80 @@
 import numpy as np
 
 
+def kernel_ee(envs1, envs2, energy_kernel, hyps, cutoffs):
+    """Compute energy/energy kernel between two structures."""
+
+    kern = 0
+    for env1 in envs1:
+        for env2 in envs2:
+            kern += \
+                energy_kernel(env1, env2, hyps, cutoffs)
+
+    return kern
+
+
+def kernel_ef(envs1, env2, component, energy_force_kernel, hyps,
+              cutoffs):
+    """Compute energy/force kernel between a structure and an environment."""
+
+    kern = 0
+    for env1 in envs1:
+        kern += \
+            energy_force_kernel(env2, env1, component, hyps, cutoffs)
+
+    return kern
+
+
 def get_ky_block(hyps, struc1, envs1, atoms1, struc2, envs2, atoms2,
                  kernel, energy_force_kernel, energy_kernel, cutoffs):
-    pass
+    """Get the covariance block of a structure comparison."""
+
+    block = np.zeros((len(struc1.labels), len(struc2.labels)))
+    index1 = 0
+    index2 = 0
+
+    if struc1.energy:
+        # energy/energy
+        if struc2.energy:
+            block[index1, index2] = \
+                kernel_ee(envs1, envs2, energy_kernel, hyps,
+                          cutoffs)
+            index2 += 1
+
+        # energy/force
+        if struc2.forces is not None:
+            for atom in atoms2:
+                force_env = envs2[atom]
+                for d in range(3):
+                    block[index1, index2] = \
+                        kernel_ef(envs1, force_env, d+1, energy_force_kernel,
+                                  hyps, cutoffs)
+                    index2 += 1
+
+    index1 += 1
+    index2 = 0
+    if struc1.forces is not None:
+        for atom in atoms1:
+            env1 = envs1[atom]
+            for d_1 in range(3):
+                # force/energy
+                if struc2.energy:
+                    block[index1, index2] = \
+                        kernel_ef(envs2, env1, d_1+1, energy_force_kernel,
+                                  hyps, cutoffs)
+                    index2 += 1
+
+                # force/force
+                if struc2.forces is not None:
+                    for env2 in envs2:
+                        for d_2 in range(3):
+                            block[index1, index2] = \
+                                kernel(env1, env2, d_1+1, d_2+1, hyps, cutoffs)
+                            index2 += 1
+
+                index1 += 1
+
+    return block
 
 
 def get_ky_mat(hyps: np.ndarray, training_strucs, training_envs,
@@ -20,62 +91,7 @@ def get_ky_mat(hyps: np.ndarray, training_strucs, training_envs,
     k_mat = np.zeros([size, size])
     no_strucs = len(training_strucs)
 
-    index1 = 0
-    for struc1_no in range(no_strucs):
-        struc1 = training_strucs[struc1_no]
-        index2 = 0
-        for struc2_no in range(struc1_no, no_strucs):
-            struc2 = training_strucs[struc2_no]
+    # covariance matrix has a block structure, where each comparison of
+    # two structures gets its own block
 
-            # energy/energy kernel
-            if struc1.energy and struc2.energy:
-                en_kern = 0
-                for env1 in training_envs[struc1_no]:
-                    for env2 in training_envs[struc2_no]:
-                        en_kern += \
-                            energy_kernel(env1, env2, hyps, cutoffs)
-                k_mat[index1, index2] = en_kern
-                index2 += 1
-            
-
-
-
-    # if structure.energy is not None:
-    #     en_kern = 0
-    #     for train_env in training_envs[struc_no]:
-    #         en_kern += \
-    #             self.energy_force_kernel(test_env, train_env, d_1,
-    #                                         self.hyps, self.cutoffs)
-    #     kernel_vector[index] = en_kern
-    #     index += 1
-
-    # if structure.forces is not None:
-    #     for atom in self.training_atoms[struc_no]:
-    #         train_env = self.training_envs[struc_no][atom]
-    #         for d_2 in range(3):
-    #             kernel_vector[index] = \
-    #                 self.kernel(test_env, train_env, d_1, d_2 + 1,
-    #                             self.hyps, self.cutoffs)
-    #             index += 1
-
-    # # calculate elements
-    # for m_index in range(size):
-    #     x_1 = training_data[int(math.floor(m_index / 3))]
-    #     d_1 = ds[m_index % 3]
-
-    #     for n_index in range(m_index, size):
-    #         x_2 = training_data[int(math.floor(n_index / 3))]
-    #         d_2 = ds[n_index % 3]
-
-    #         # calculate kernel and gradient
-    #         kern_curr = kernel(x_1, x_2, d_1, d_2, hyps,
-    #                            cutoffs)
-
-    #         # store kernel value
-    #         k_mat[m_index, n_index] = kern_curr
-    #         k_mat[n_index, m_index] = kern_curr
-
-    # # matrix manipulation
-    # ky_mat = k_mat + sigma_n ** 2 * np.eye(size)
-
-    # return ky_mat
+    pass
