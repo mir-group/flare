2f73f8618c984d2c2905a7788087dee9d9ea39d3
Lixin Sun <lixinsun@g.harvard.edu>
2019-11-12 18:52:10 -0500

    adjust the partition block size

--- tests/test_gp_algebra.py
+++ tests/test_gp_algebra.py
@@ -1,30 +1,25 @@
+import time
 import pytest
-import pickle
-import os
-import json
 import numpy as np
 
-from pytest import raises
-
-from flare.gp import GaussianProcess
+from flare import gp_algebra, gp_algebra_multi
 from flare.env import AtomicEnvironment
 from flare.struc import Structure
-from flare import mc_simple
-from flare.otf_parser import OtfAnalysis
-from flare.env import AtomicEnvironment
-from flare.mc_simple import two_plus_three_body_mc, two_plus_three_body_mc_grad
-from flare.mc_sephyps import two_plus_three_body_mc as two_plus_three_body_mc_multi
-from flare.mc_sephyps import two_plus_three_body_mc_grad as two_plus_three_body_mc_grad_multi
-from flare import gp_algebra, gp_algebra_multi
+from flare.mc_simple import two_plus_three_body_mc, \
+        two_plus_three_body_mc_grad
+from flare.mc_sephyps import two_plus_three_body_mc \
+        as two_plus_three_body_mc_multi
+from flare.mc_sephyps import two_plus_three_body_mc_grad \
+        as two_plus_three_body_mc_grad_multi
 
 
 def get_random_training_set(nenv):
-    """Create a random test structure """
+    """Create a random training_set array with parameters """
 
     np.random.seed(0)
 
     cutoffs = np.array([0.8, 0.8])
-    hyps = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1])
+    hyps = np.ones(5, dtype=float)
     kernel = (two_plus_three_body_mc, two_plus_three_body_mc_grad)
     kernel_m = (two_plus_three_body_mc_multi, two_plus_three_body_mc_grad_multi)
     hyps_mask = {'nspec': 2,
@@ -57,7 +52,7 @@ def test_ky_mat():
 
     hyps, training_data, training_labels, kernel, cutoffs, \
             kernel_m, hyps_mask = \
-            get_random_training_set(10)
+            get_random_training_set(50)
 
 
     func = [gp_algebra.get_ky_mat,
@@ -65,14 +60,30 @@ def test_ky_mat():
             gp_algebra_multi.get_ky_mat,
             gp_algebra_multi.get_ky_mat_par]
 
+    # get the reference
+    # timer0 = time.time()
     ky_mat0 = func[0](hyps, training_data,
                        training_labels, kernel[0], cutoffs)
+    # print("linear", time.time()-timer0)
+
     # parallel version
     ky_mat = func[1](hyps, training_data,
-                     training_labels, kernel[0], cutoffs, no_cpus=2)
+                     training_labels, kernel[0], cutoffs,
+                     no_cpus=2, nsample=20)
     diff = (np.max(np.abs(ky_mat-ky_mat0)))
     assert (diff==0), "parallel implementation is wrong"
 
+    # this part of the code can be use for timing the parallel performance
+    # for n in [10, 50, 100]:
+    #     timer0 = time.time()
+    #     ky_mat = func[1](hyps, training_data,
+    #                      training_labels, kernel[0], cutoffs,
+    #                      no_cpus=8, nsample=n)
+    #     diff = (np.max(np.abs(ky_mat-ky_mat0)))
+    #     print("parallel", n, time.time()-timer0, diff)
+    #     assert (diff==0), "parallel implementation is wrong"
+
+    hyps = np.ones(9, dtype=float)
     # check multi hyps implementation
     ky_mat = func[2](hyps, training_data,
                       training_labels, kernel_m[0], cutoffs, hyps_mask)
@@ -82,7 +93,7 @@ def test_ky_mat():
     # check multi hyps parallel implementation
     ky_mat = func[3](hyps, training_data,
                      training_labels, kernel_m[0], cutoffs,
-                     hyps_mask, no_cpus=2)
+                     hyps_mask, no_cpus=2, nsample=20)
     diff = (np.max(np.abs(ky_mat-ky_mat0)))
     assert (diff==0), "multi hyps parameter parallel "\
             "implementation is wrong"
@@ -107,6 +118,7 @@ def test_ky_and_hyp():
     diff = (np.max(np.abs(ky_mat-ky_mat0)))
     assert (diff==0), "parallel implementation is wrong"
 
+    hyps = np.ones(9, dtype=float)
     # check multi hyps implementation
     hypmat, ky_mat = func[2](hyps, hyps_mask, training_data,
                       training_labels, kernel_m[1], cutoffs)
