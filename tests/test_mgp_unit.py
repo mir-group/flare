b8b0ed842e285ce894e0198a6bfc3670a227fc6d
Lixin Sun <lixinsun@g.harvard.edu>
2019-11-13 13:17:44 -0500

    add unittest for mgp

--- tests/test_mgp_unit.py
+++ tests/test_mgp_unit.py
@@ -17,10 +17,41 @@ import os
                                   'in environment: Please install LAMMPS '
                                   'and set the $lmp env. '
                                   'variable to point to the executatble.')
-def test_parse_header():
-    # -------------------------------------------------------------------------
-    #                  reconstruct gp model from otf snippet
-    # -------------------------------------------------------------------------
+
+def construct_gp(bodies, params):
+
+    unique_species, hyps, cutoffs, noa, nenv, cell = params
+
+    # construct a 2b gp model
+    if bodies==2:
+        kernel = mc_simple.two_body_mc #plus_three_body_mc
+        kernel_grad = mc_simple.two_body_mc_grad # plus_three_body_mc_grad
+    if bodies == 3:
+        kernel = mc_simple.three_body_mc #plus_three_body_mc
+        kernel_grad = mc_simple.three_body_mc_grad # plus_three_body_mc_grad
+
+    gp_model = gp.GaussianProcess(kernel, kernel_grad, hyps,
+                           cutoffs, par=True, no_cpus=2)
+    gp_model.hyp_labels = ['sig2', 'ls2', 'noise']
+
+    # create training_set
+    gp_model.training_data = []
+    gp_model.training_labels_np = []
+    for idenv in range(nenv):
+        positions = np.random.uniform(-1, 1, [noa,3])
+        forces = np.random.uniform(-1, 1, [noa,3])
+        species = [unique_species[i] for i in \
+                np.random.randint(0, len(unique_species), noa)]
+        structure = struc.Structure(cell, species, positions, forces=forces)
+        gp_model.update_db(structure, forces, [1])
+    test_envi = env.AtomicEnvironment(structure, 1, cutoffs)
+    struc_test = structure
+    gp_model.set_L_alpha()
+    return gp_model, test_envi, struc_test
+
+
+@pytest.mark.parametrize('bodies', [ 2, 3])
+def test_parse_header(bodies):
 
     unique_species = [2, 1]
     hyps = [1, 1, 0.01]
@@ -29,50 +60,9 @@ def test_parse_header():
     nenv=10
     cell = np.eye(3)
 
-    # construct a 2b gp model
-    kernel = mc_simple.two_body_mc #plus_three_body_mc
-    kernel_grad = mc_simple.two_body_mc_grad # plus_three_body_mc_grad
-    gp_2b = gp.GaussianProcess(kernel, kernel_grad, hyps,
-                           cutoffs, par=True, no_cpus=2)
-    gp_2b.hyp_labels = ['sig2', 'ls2', 'noise']
+    params = (unique_species, hyps, cutoffs, noa, nenv, cell)
 
-    # create training_set
-    gp_2b.training_data = []
-    gp_2b.training_labels_np = []
-    for idenv in range(nenv):
-        positions = np.random.uniform(-1, 1, [noa,3])
-        forces = np.random.uniform(-1, 1, [noa,3])
-        species = [unique_species[i] for i in \
-                np.random.randint(0, len(unique_species), noa)]
-        structure = struc.Structure(cell, species, positions, forces=forces)
-        gp_2b.update_db(structure, forces, [1])
-    test2b_envi = env.AtomicEnvironment(structure, 1, cutoffs)
-    structure_2b = structure
-    gp_2b.set_L_alpha()
-
-    # construct a 3b gp model
-    kernel = mc_simple.three_body_mc #plus_three_body_mc
-    kernel_grad = mc_simple.three_body_mc_grad # plus_three_body_mc_grad
-    gp_3b = gp.GaussianProcess(kernel, kernel_grad, hyps,
-                           cutoffs, par=True, no_cpus=2)
-    gp_3b.hyp_labels = ['sig2', 'ls2', 'noise']
-
-    # create training_set
-    gp_3b.training_data = []
-    gp_3b.training_labels_np = []
-    for idenv in range(nenv):
-        positions = np.random.uniform(-1, 1, [noa,3])
-        forces = np.random.uniform(-1, 1, [noa,3])
-        species = [unique_species[i] for i in \
-                np.random.randint(0, len(unique_species), noa)]
-        structure = struc.Structure(cell, species, positions)
-        gp_3b.update_db(structure, forces, [1])
-    test3b_envi = env.AtomicEnvironment(structure, 1, cutoffs)
-    structure_3b = structure
-    gp_3b.set_L_alpha()
-    # -------------------------------------------------------------------------
-    #                              map the potential
-    # -------------------------------------------------------------------------
+    gp_model, test_envi, struc_test = construct_gp(bodies, params)
 
     grid_num_2 = 128
     grid_num_3 = 15
@@ -95,20 +85,20 @@ def test_parse_header():
                    'grid_num_3': [grid_num_3, grid_num_3, grid_num_3],
                    'svd_rank_2': 64,
                    'svd_rank_3': 90,
-                   'bodies': [2],
+                   'bodies': [bodies],
                    'load_grid': None,
                    'update': True}
 
-    mgp_2b = MappedGaussianProcess(gp_2b.hyps, gp_2b.cutoffs,
+    mgp_model = MappedGaussianProcess(gp_model.hyps, gp_model.cutoffs,
             grid_params, struc_params, mean_only=True, container_only=False,
-            GP=gp_2b, lmp_file_name=lammps_location)
+            GP=gp_model, lmp_file_name=lammps_location)
 
     # -------------------------------------------------------------------------
     #                          test the mapped potential
     # -------------------------------------------------------------------------
 
-    gp_pred_x = gp_2b.predict(test2b_envi, 1)
-    mgp_pred = mgp_2b.predict(test2b_envi, mean_only=True)
+    gp_pred_x = gp_model.predict(test_envi, 1)
+    mgp_pred = mgp_model.predict(test_envi, mean_only=True)
 
     # check mgp is within 1 meV/A of the gp
     assert(np.abs(mgp_pred[0][0] - gp_pred_x[0]) < 1e-3)
@@ -116,17 +106,17 @@ def test_parse_header():
     #                           check lammps potential
     # -------------------------------------------------------------------------
 
-    # mgp_2b.write_lmp_file(lammps_location)
+    # mgp_model.write_lmp_file(lammps_location)
     # lmp file is automatically written now every time MGP is constructed
 
     # create test structure
 
     atom_types = [1, 2]
     atom_masses = [108, 127]
-    atom_species = structure_2b.coded_species
+    atom_species = struc_test.coded_species
     # create data file
     data_file_name = 'tmp.data'
-    data_text = lammps_calculator.lammps_dat(structure_2b, atom_types,
+    data_text = lammps_calculator.lammps_dat(struc_test, atom_types,
                                              atom_masses, atom_species)
     lammps_calculator.write_text(data_file_name, data_text)
 
@@ -146,7 +136,7 @@ def test_parse_header():
                                  output_file_name)
 
     lammps_forces = lammps_calculator.lammps_parser(dump_file_name)
-    mgp_forces = mgp_2b.predict(test2b_envi, mean_only=True)
+    mgp_forces = mgp_model.predict(test_envi, mean_only=True)
 
     # check that lammps agrees with gp to within 1 meV/A
     assert(np.abs(lammps_forces[1, 1] - mgp_forces[0][1]) < 1e-3)
@@ -155,4 +145,4 @@ def test_parse_header():
               ' log.lammps')
     os.system('rm '+lammps_location)
     os.system('rm grid3*.npy')
-    # os.system(f'rm -r {mgp_2b.kv3name}')
+    # os.system(f'rm -r {mgp_model.kv3name}')
