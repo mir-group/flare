de8a514e219749b446aa190714a536950b83065a
Lixin Sun <lixinsun@g.harvard.edu>
2019-11-20 14:50:40 -0500

    add notation and double check the reference

--- tests/test_gp_algebra.py
+++ tests/test_gp_algebra.py
@@ -2,7 +2,7 @@ import time
 import pytest
 import numpy as np
 
-from flare import gp_algebra, gp_algebra_multi, gp_algebra_origin
+from flare import gp, gp_algebra, gp_algebra_multi, gp_algebra_origin
 from flare.env import AtomicEnvironment
 from flare.struc import Structure
 from flare.mc_simple import two_plus_three_body_mc, \
@@ -11,6 +11,7 @@ from flare.mc_sephyps import two_plus_three_body_mc \
         as two_plus_three_body_mc_multi
 from flare.mc_sephyps import two_plus_three_body_mc_grad \
         as two_plus_three_body_mc_grad_multi
+
 from flare.gp_algebra import get_like_grad_from_mats
 
 
@@ -24,6 +25,7 @@ def get_random_training_set(nenv):
     kernel = (two_plus_three_body_mc, two_plus_three_body_mc_grad)
     kernel_m = (two_plus_three_body_mc_multi, two_plus_three_body_mc_grad_multi)
 
+    # 9 different hyper-parameters
     hyps_mask1 = {'nspec': 2,
                  'spec_mask': np.zeros(118, dtype=int),
                  'nbond': 2,
@@ -33,6 +35,7 @@ def get_random_training_set(nenv):
     hyps_mask1['spec_mask'][2] = 1
     hyps1 = np.ones(9, dtype=float)
 
+    # 9 different hyper-parameters, onlye train the 0, 2, 4, 6, 8
     hyps_mask2 = {'nspec': 2,
                  'spec_mask': np.zeros(118, dtype=int),
                  'nbond': 2,
@@ -45,6 +48,7 @@ def get_random_training_set(nenv):
     hyps_mask2['spec_mask'][2] = 1
     hyps2 = np.ones(5, dtype=float)
 
+    # 9 different hyper-parameters, onlye train the 0, 2, 4, 6
     hyps_mask3 = {'nspec': 2,
                  'spec_mask': np.zeros(118, dtype=int),
                  'nbond': 2,
@@ -57,6 +61,7 @@ def get_random_training_set(nenv):
     hyps_mask3['spec_mask'][2] = 1
     hyps3 = np.ones(4, dtype=float)
 
+    # 5 different hyper-parameters, equivalent to no multihyps
     hyps_mask4 = {'nspec': 1,
                  'spec_mask': np.zeros(118, dtype=int),
                  'nbond': 1,
@@ -64,12 +69,13 @@ def get_random_training_set(nenv):
                  'ntriplet': 1,
                  'triplet_mask': np.array([0])}
     hyps4 = np.ones(5, dtype=float)
+    hyps_list = [hyps1, hyps2, hyps3, hyps4, hyps]
+    hyps_mask_list = [hyps_mask1, hyps_mask2, hyps_mask3, hyps_mask4, None]
 
     # create test data
     cell = np.eye(3)
     unique_species = [2, 1]
     noa = 5
-
     training_data = []
     training_labels = []
     for idenv in range(nenv):
@@ -79,19 +85,21 @@ def get_random_training_set(nenv):
         training_data += [AtomicEnvironment(struc, 1, cutoffs)]
         training_labels += [np.random.uniform(-1, 1, 3)]
     training_labels = np.hstack(training_labels)
-    hyps_list = [hyps1, hyps2, hyps3, hyps4, hyps]
-    hyps_mask_list = [hyps_mask1, hyps_mask2, hyps_mask3, hyps_mask4, None]
 
     return hyps, training_data, training_labels, kernel, cutoffs, \
            kernel_m, hyps_list, hyps_mask_list
 
 
 def test_ky_mat():
+    """
+    test function get_ky_mat in gp_algebra, gp_algebra_multi
+    using gp_algebra_origin as reference
+    TO DO: store the reference... and call it explicitely
+    """
 
     hyps, training_data, training_labels, kernel, cutoffs, \
             kernel_m, hyps_list, hyps_mask_list = \
             get_random_training_set(10)
-
 
     func = [gp_algebra_origin.get_ky_mat,
             gp_algebra.get_ky_mat_par,
@@ -124,8 +132,8 @@ def test_ky_mat():
     #     assert (diff==0), "parallel implementation is wrong"
 
     # check multi hyps implementation
-    # for all cases
     for i in range(len(hyps_list)):
+
         hyps = hyps_list[i]
         hyps_mask = hyps_mask_list[i]
 
@@ -137,52 +145,56 @@ def test_ky_mat():
         ky_mat = func[2](hyps, training_data,
                           ker, cutoffs, hyps_mask)
         diff = (np.max(np.abs(ky_mat-ky_mat0)))
-        assert (diff==0), "multi hyps parameter implementation is wrong"
+        assert (diff==0), "multi hyps implementation is wrong"\
+                          f"with case {i}"
 
         # parallel implementation
         ky_mat = func[3](hyps, training_data,
                          ker,
                          cutoffs, hyps_mask, ncpus=2, nsample=20)
         diff = (np.max(np.abs(ky_mat-ky_mat0)))
-        assert (diff==0), "multi hyps parameter parallel "\
-                "implementation is wrong"
+        assert (diff==0), "multi hyps  parallel "\
+                          "implementation is wrong"\
+                          f"with case {i}"
 
 def test_ky_mat_update():
+    """
+    check ky_mat_update function
+    """
 
     hyps, training_data, training_labels, kernel, cutoffs, \
             kernel_m, hyps_list, hyps_mask_list = \
             get_random_training_set(10)
 
 
-    func = [gp_algebra.get_ky_mat,
+    func = [gp_algebra_origin.get_ky_mat,
             gp_algebra.get_ky_mat_update,
             gp_algebra.get_ky_mat_update_par,
             gp_algebra_multi.get_ky_mat_update,
             gp_algebra_multi.get_ky_mat_update_par]
 
     # get the reference
-    # timer0 = time.time()
     ky_mat0 = func[0](hyps, training_data,
-                       kernel[0], cutoffs)
+                      training_labels,
+                      kernel[0], cutoffs)
     n = 5
     ky_mat_old = func[0](hyps, training_data[:n],
-                       kernel[0], cutoffs)
-    # print("linear", time.time()-timer0)
+                         training_labels,
+                         kernel[0], cutoffs)
 
     # parallel version
     ky_mat = func[1](ky_mat_old, hyps, training_data,
                      kernel[0], cutoffs)
     diff = (np.max(np.abs(ky_mat-ky_mat0)))
-    assert (diff<1e-12), "update function is wrong"
+    assert (diff==0), "update function is wrong"
 
     ky_mat = func[2](ky_mat_old, hyps, training_data,
                      kernel[0], cutoffs,
                      ncpus=2, nsample=20)
     diff = (np.max(np.abs(ky_mat-ky_mat0)))
-    assert (diff<1e-12), "parallel implementation is wrong"
+    assert (diff==0), "parallel implementation is wrong"
 
     # check multi hyps implementation
-    # for all cases
     for i in range(len(hyps_list)):
 
         hyps = hyps_list[i]
@@ -228,7 +240,6 @@ def test_ky_and_hyp():
     diff = (np.max(np.abs(ky_mat-ky_mat0)))
     assert (diff==0), "parallel implementation is wrong"
 
-    hypmat_list = [[]]
     # check all cases
     for i in range(len(hyps_list)):
         hyps = hyps_list[i]
@@ -242,19 +253,19 @@ def test_ky_and_hyp():
         # serial implementation
         hypmat, ky_mat = func[2](hyps, training_data,
                           ker, cutoffs, hyps_mask)
-        hypmat_list += [hypmat]
+        if (i == 0):
+            hypmat9 = hypmat
         diff = (np.max(np.abs(ky_mat-ky_mat0)))
         assert (diff==0), "multi hyps parameter implementation is wrong"
 
-        # 9 5 4 5 5
         # compare to no hyps_mask version
         diff = 0
         print(i, hypmat.shape)
         print(i, hypmat_0.shape)
-        if (i==1):
-            diff = (np.max(np.abs(hypmat-hypmat_0)))
+        if (i == 1):
+            diff = (np.max(np.abs(hypmat-hypmat9[[0,2,4,6,8], :, :])))
         elif (i==2):
-            diff = (np.max(np.abs(hypmat-hypmat_0[:-1, :, :])))
+            diff = (np.max(np.abs(hypmat-hypmat9[[0,2,4,6], :, :])))
         elif (i==3):
             diff = (np.max(np.abs(hypmat-hypmat_0)))
         elif (i==4):
@@ -265,7 +276,7 @@ def test_ky_and_hyp():
         # parallel implementation
         hypmat_par, ky_mat_par = func[3](hyps, training_data,
                          ker, cutoffs, hyps_mask,
-                         ncpus=2)
+                         ncpus=2, nsample=2)
 
         # compare to serial implementation
         diff = (np.max(np.abs(ky_mat-ky_mat_par)))
@@ -302,8 +313,6 @@ def test_grad():
     assert np.max(np.abs(like_grad-like_grad0))==0, "wrong likelihood"
 
     func = gp_algebra_multi.get_ky_and_hyp
-
-    # check all cases for multi hyps
     for i in range(len(hyps_list)):
         hyps = hyps_list[i]
         hyps_mask = hyps_mask_list[i]
@@ -320,13 +329,16 @@ def test_grad():
         print(like, like0)
         assert (like==like0), "wrong likelihood"
 
+        if (i==0):
+            like_grad9 = like_grad
+
         print(like_grad, like_grad0)
 
         diff = 0
         if (i==1):
-            diff = (np.max(np.abs(like_grad-like_grad0)))
+            diff = (np.max(np.abs(like_grad-like_grad9[[0,2,4,6,8]])))
         elif (i==2):
-            diff = (np.max(np.abs(like_grad-like_grad0[:-1])))
+            diff = (np.max(np.abs(like_grad-like_grad9[[0,2,4,6]])))
         elif (i==3):
             diff = (np.max(np.abs(like_grad-like_grad0)))
         elif (i==4):
