e5eccbd0ee43d0b87cfe0e4ed1d46c136c60ee4a
Lixin Sun <lixinsun@g.harvard.edu>
2019-11-22 13:31:29 -0500

    change run_dft to run_dft_par

--- tests/test_qe_util.py
+++ tests/test_qe_util.py
@@ -3,7 +3,7 @@ import os
 import sys
 import numpy as np
 from flare.struc import Structure, get_unique_species
-from flare.dft_interface.qe_util import parse_dft_input, parse_dft_forces, run_dft, \
+from flare.dft_interface.qe_util import parse_dft_input, parse_dft_forces, run_dft_par, \
     edit_dft_input_positions, dft_input_to_structure
 
 def cleanup_espresso_run(target: str = None):
@@ -91,8 +91,7 @@ def test_espresso_calling(qe_input, qe_output):
                           positions=positions,
                           mass_dict=masses, species_labels=species)
 
-    forces = run_dft('pwscf.in',
-                          structure, dft_loc)
+    forces = run_dft_par('pwscf.in', structure, dft_loc, dft_out=qe_output)
 
     ref_forces = parse_dft_forces(qe_output)
 
