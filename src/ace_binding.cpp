0999279c1764c88800e0485d1c7fe928761d1653
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-12-11 15:44:04 -0500

    start structure binding

--- src/ace_binding.cpp
+++ src/ace_binding.cpp
@@ -2,14 +2,13 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 namespace py = pybind11;
-using namespace std;
 
 // Define spherical harmonics class.
 class SphericalHarmonics{
     public:
         double x, y, z;
         int lmax;
-        vector<double> Y, Yx, Yy, Yz;
+        std::vector<double> Y, Yx, Yy, Yz;
 
         SphericalHarmonics(double x, double y, double z, int lmax);
 };
@@ -24,15 +23,16 @@ SphericalHarmonics :: SphericalHarmonics(double x, double y, double z,
     this->lmax = lmax;
 
     // Initialize spherical harmonic vectors.
-    Y = vector<double>(number_of_harmonics, 0);
-    Yx = vector<double>(number_of_harmonics, 0);
-    Yy = vector<double>(number_of_harmonics, 0);
-    Yz = vector<double>(number_of_harmonics, 0);
+    Y = std::vector<double>(number_of_harmonics, 0);
+    Yx = std::vector<double>(number_of_harmonics, 0);
+    Yy = std::vector<double>(number_of_harmonics, 0);
+    Yz = std::vector<double>(number_of_harmonics, 0);
 
     get_Y(Y, Yx, Yy, Yz, x, y, z, lmax);
 };
 
 PYBIND11_MODULE(ace, m){
+    // Bind the spherical harmonics class.
     py::class_<SphericalHarmonics>(m, "SphericalHarmonics")
         .def(py::init<double, double, double, int>())
         // Make attributes accessible.
@@ -44,4 +44,21 @@ PYBIND11_MODULE(ace, m){
         .def_readwrite("Yx", &SphericalHarmonics::Yx)
         .def_readwrite("Yy", &SphericalHarmonics::Yy)
         .def_readwrite("Yz", &SphericalHarmonics::Yz);
+    
+    // Bind the structure class.
+    py::class_<Structure>(m, "Structure")
+        .def(py::init<const std::vector<double> &,
+                      const std::vector<double> &,
+                      const std::vector<double> &,
+                      const std::vector<double> &,
+                      const std::vector<double> &,
+                      const std::vector<double> &,
+                      const std::vector<int> &>())
+        .def_readwrite("xs", &Structure::xs)
+        .def_readwrite("ys", &Structure::ys)
+        .def_readwrite("zs", &Structure::zs)
+        .def_readwrite("vec1", &Structure::vec1)
+        .def_readwrite("vec2", &Structure::vec2)
+        .def_readwrite("vec3", &Structure::vec3)
+        .def_readwrite("species", &Structure::species);
 }
