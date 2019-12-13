d551d5a650f6e79627cb67fb0715e091f90e3ec9
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-12-12 20:52:01 -0500

    add wrap test

--- test/test_structure.cpp
+++ test/test_structure.cpp
@@ -2,29 +2,45 @@
 #include "ace.h"
 #include <iostream>
 #include <cmath>
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 
-using namespace std;
-using namespace Eigen;
+TEST(StructureTest, TestWrapped){
+    // Check that the wrapped coordinates are equivalent to Cartesian coordinates up to lattice translations.
 
-TEST(StructureTest, EigenTest){
-    MatrixXd m = MatrixXd::Random(3,3);
-    cout << "let's take the inverse." << endl;
-    MatrixXd m_inv = m.inverse();
-    cout << m_inv << endl;
-    cout <<  "let's try matrix multiplication." << endl;
-    cout << m * m_inv << endl;
-    EXPECT_EQ(1, 1);
+    Eigen::MatrixXd cell(3, 3);
+    Eigen::MatrixXd positions(5, 3);
+
+    // Create arbitrary structure.
+    cell << 1.3, 0.5, 0.8,
+            -1.2, 1, 0.73,
+            -0.8, 0.1, 0.9;
+
+    std::vector<int> species {1, 2, 3, 4, 5};
+
+    positions << 1.2, 0.7, 2.3,
+                 3.1, 2.5, 8.9,
+                 -1.8, -5.8, 3.0,
+                 0.2, 1.1, 2.1,
+                 3.2, 1.1, 3.3;
+
+    Structure test_struc = 
+        Structure(cell, species, positions);
+    
+    // Take positions minus wrapped positions.
+    Eigen::MatrixXd wrap_diff =
+        test_struc.positions - test_struc.wrapped_positions;
+    
+    Eigen::MatrixXd wrap_rel =
+        (wrap_diff * test_struc.cell_transpose) *
+        test_struc.cell_dot_inverse;
+
+    // Check that it maps to the origin under lattice translations.
+    Eigen::MatrixXd check_lat =
+        wrap_rel.array().round() - wrap_rel.array();
+
+    for (int i = 0; i < 5; i ++){
+        for (int j = 0; j < 3; j ++){
+            EXPECT_LE(abs(check_lat(i,j)), 1e-10);
+        }
+    }
 }
-
-// TEST(StructureTest, ConstructorTest){
-//     vector<double> xs {1, 2, 3};
-//     vector<double> ys {4, 5, 6};
-//     vector<double> zs {7, 8, 9};
-//     vector<double> vec1 {1, 0, 0};
-//     vector<double> vec2 {0, 1, 0};
-//     vector<double> vec3 {0, 0, 1};
-//     vector<int> species {1, 2, 3};
-//     Structure test = Structure(xs, ys, zs, vec1, vec2, vec3, species);
-//     cout << test.xs[2];
-// }
