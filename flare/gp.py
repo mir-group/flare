92a5ac42894c223425705af51b3a787c99b6553d
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2019-12-15 17:33:47 -0500

    remove gp assertion

--- flare/gp.py
+++ flare/gp.py
@@ -234,9 +234,9 @@ class GaussianProcess:
         # Kernel vector allows for evaluation of At. Env.
         k_v = self.get_kernel_vector(x_t, d)
 
-        # Guarantee that alpha is up to date with training set
-        assert ((self.alpha is not None) and
-                (3 * len(self.training_data) == len(self.alpha)))
+        # # Guarantee that alpha is up to date with training set
+        # assert ((self.alpha is not None) and
+        #         (3 * len(self.training_data) == len(self.alpha)))
 
         # get predictive mean
         pred_mean = np.matmul(k_v, self.alpha)
