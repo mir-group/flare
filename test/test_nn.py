d18b200c52891da5461116beda6e2d915d8269b7
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-01-13 14:53:11 -0500

    implement bpnnp

--- test/test_nn.py
+++ test/test_nn.py
@@ -2,6 +2,7 @@ import ace
 import nnp
 import numpy as np
 import torch
+import copy
 
 species = [6, 6, 6, 6, 6, 6, 6, 8, 8, 8, 6, 6, 8, 1, 1, 1, 1, 1, 1, 1, 1]
 
@@ -81,8 +82,79 @@ test2 = nnp_test.predict_local_EFS(test_env)
 test1[1].backward()
 test2[1].backward()
 
-# # Torch tests.
-# x = torch.tensor([1., 2., 3.], requires_grad = True)
-# y = torch.sum(x * x)
-# z = torch.autograd.grad(y, x, create_graph=True)
-# z[0].sum().backward()
+# ------------------------
+#  Test NN force
+# ------------------------
+
+cell = np.eye(3) * 10
+species = [0, 0, 1, 2]
+positions = np.array([[0, 0, 0],
+                      [1, 0.2, -0.1],
+                      [0.4, -0.3, 0.2],
+                      [0.1, 0.1, -0.3]])
+delt = 1e-2
+atom = 1
+comp = 1
+pos_delt = copy.copy(positions)
+pos_delt[atom, comp] += delt
+
+pos_delt_2 = copy.copy(positions)
+pos_delt_2[atom, comp] -= delt
+
+test_struc = ace.Structure(cell, species, positions)
+struc_delt = ace.Structure(cell, species, pos_delt)
+struc_delt_2 = ace.Structure(cell, species, pos_delt_2)
+
+test_F = nnp_test.predict_F(test_struc)
+test_E = nnp_test.predict_E(test_struc)
+E_delt = nnp_test.predict_E(struc_delt)
+E_delt_2 = nnp_test.predict_E(struc_delt_2)
+
+force_an = test_F[atom * 3 + comp].item()
+force_delt = -((E_delt - E_delt_2) / (2 * delt))[0].item()
+
+print(force_an)
+print(force_delt)
+print(force_delt - force_an)
+
+# Check parameter gradients.
+param_list = list(nnp_test.parameters())
+nnp_test.zero_grad()
+# print(param_list[0].grad)
+# test_F = nnp_test.predict_F(test_struc)
+random_target = torch.randn(len(positions) * 3).double()
+nnp_test.update_weights(test_struc, random_target)
+# print(param_list)
+
+# print('\nspec0 parameters\n')
+# print(list(nnp_test.spec0.parameters())[0])
+
+# # Test autograd function.
+# x = torch.tensor([1., 2., 3.], requires_grad=True)
+# y = (x * x * x).sum()
+# z = torch.autograd.grad(y, x, create_graph=True)[0]
+# z.sum().backward()
+# print(x.grad)
+
+x = torch.zeros(2)
+y = torch.tensor([1., 2.], requires_grad=True)
+z = torch.tensor([4., 5.], requires_grad=True)
+x += y * z
+x += y * z * y * z
+s = x.sum()
+s.backward()
+print(z.grad)
+
+# print(nnp_test.spec0.forward(torch.randn(10, 6).double()))
+
+# # Test NN gradients
+# print(list(nnp_test.parameters())[0])
+# print(list(nnp_test.parameters())[0].grad)
+
+# x = torch.tensor([5.], requires_grad=True)
+# y = x * x * x
+# y2 = x * x
+# z = torch.autograd.grad(y, x, create_graph=True)[0]
+# z2 = torch.autograd.grad(y2, x, create_graph=True)[0]
+# (z + z2).backward()
+# print(x.grad)
