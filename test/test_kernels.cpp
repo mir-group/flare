7bbb5a6a76d3c0d11bed80f3f3535278a504f975
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-02-03 16:27:58 -0500

    add kernel stress test

--- test/test_kernels.cpp
+++ test/test_kernels.cpp
@@ -9,8 +9,8 @@
 class KernelTest : public ::testing::Test{
     public:
         // structure
-        Eigen::MatrixXd cell{3, 3};
-        std::vector<int> species {0, 0, 0, 0, 0};
+        Eigen::MatrixXd cell{3, 3}, cell_2{3, 3};
+        std::vector<int> species {0, 1, 0, 1, 0};
         Eigen::MatrixXd positions{5, 3}, positions_2{5, 3}, positions_3{5, 3};
         StructureDescriptor test_struc, test_struc_2, test_struc_3;
 
@@ -22,7 +22,7 @@ class KernelTest : public ::testing::Test{
         std::string cutoff_string = "cosine";
         std::vector<double> radial_hyps {0, 5};
         std::vector<double> cutoff_hyps;
-        std::vector<int> descriptor_settings {1, 10, 10};
+        std::vector<int> descriptor_settings {2, 10, 10};
         double cutoff = 5;
         DescriptorCalculator desc1;
 
@@ -30,6 +30,7 @@ class KernelTest : public ::testing::Test{
         double signal_variance = 1;
         double power = 2;
         DotProductKernel kernel;
+        Eigen::VectorXd kern_vec;
 
     KernelTest(){
         cell << 10, 0, 0,
@@ -58,6 +59,8 @@ class KernelTest : public ::testing::Test{
         test_env = test_struc_2.environment_descriptors[0];
 
         kernel = DotProductKernel(signal_variance, power);
+
+        kern_vec = kernel.env_struc(test_env, test_struc);
     }
 };
 
@@ -69,9 +72,6 @@ TEST_F(KernelTest, NormTest){
 }
 
 TEST_F(KernelTest, ForceTest){
-    // Calculate unperturbed kernel.
-    Eigen::VectorXd kern_vec = kernel.env_struc(test_env, test_struc);
-
     // Perturb the coordinates of the environment atoms.
     double thresh = 1e-6;
     int noa = 5;
@@ -90,6 +90,43 @@ TEST_F(KernelTest, ForceTest){
             abs_diff = abs(fin_val - exact_val); 
 
             EXPECT_NEAR(abs_diff, 0, thresh);
+        }
+    }
+}
+
+TEST_F(KernelTest, StressTest){
+    double thresh = 1e-6;
+    int noa = 5;
+    double delta = 1e-8;
+    Eigen::VectorXd kern_pert;
+    double fin_val, exact_val, abs_diff;
+    int stress_count = 0;
+    // Test all 6 independent strains (xx, xy, xz, yy, yz, zz).
+    for (int m = 0; m < 3; m ++){
+        for (int n = m; n < 3; n ++){
+            cell_2 = cell;
+            positions_3 = positions;
+
+            // Perform strain.
+            cell_2(0, m) += cell(0, n) * delta;
+            cell_2(1, m) += cell(1, n) * delta;
+            cell_2(2, m) += cell(2, n) * delta;
+            for (int k = 0; k < noa; k ++){
+                positions_3(k, m) += positions(k, n) * delta;
+            }
+
+            test_struc_3 = StructureDescriptor(cell_2, species, positions_3,
+                                               desc1, cutoff);
+
+            kern_pert = kernel.env_struc(test_env, test_struc_3);
+            fin_val = -(kern_pert(0) - kern_vec(0)) / delta;
+            exact_val = kern_vec(1 + 3 * noa + stress_count)
+                * test_struc.volume;
+            abs_diff = abs(fin_val - exact_val); 
+
+            EXPECT_NEAR(abs_diff, 0, thresh);
+
+            stress_count ++;
         }
     }
 }
