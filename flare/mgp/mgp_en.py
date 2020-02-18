dd863e96ad3f318887d920ed4d7a664806af2322
Yu Shelby Xie <xieyu.fish@gmail.com>
2020-02-17 22:39:01 -0500

    move write_2/3_body into the sub-classes

--- flare/mgp/mgp_en.py
+++ flare/mgp/mgp_en.py
@@ -347,61 +347,6 @@ class MappedGaussianProcess:
             v = mapping.var.V @ v_d
         return f, vir, v, e
 
-    def write_two_body(self, f):
-        """
-        Write two-body components to a file handle f.
-        :param f:
-        :return:
-        """
-        a = self.bounds_2[0][0]
-        b = self.bounds_2[1][0]
-        order = self.grid_num_2
-
-        for ind, spc in enumerate(self.spcs[0]):
-            coefs_2 = self.maps_2[ind].mean.__coeffs__
-
-            elem1 = Z_to_element(spc[0])
-            elem2 = Z_to_element(spc[1])
-
-            header_2 = f'{elem1} {elem2} {a} {b} {order}\n'
-            f.write(header_2)
-
-            for c, coef in enumerate(coefs_2):
-                f.write(f'{coef:.10e} ')
-                if c % 5 == 4 and c != len(coefs_2)-1:
-                    f.write('\n')
-
-            f.write('\n')
-
-    def write_three_body(self, f):
-        a = self.bounds_3[0]
-        b = self.bounds_3[1]
-        order = self.grid_num_3
-
-        for ind, spc in enumerate(self.spcs[1]):
-            coefs_3 = self.maps_3[ind].mean.__coeffs__
-
-            elem1 = Z_to_element(spc[0])
-            elem2 = Z_to_element(spc[1])
-            elem3 = Z_to_element(spc[2])
-
-            header_3 = f'{elem1} {elem2} {elem3} {a[0]} {a[1]} {a[2]}'\
-                       f' {b[0]} {b[1]} {b[2]:.10e}'\
-                       f' {order[0]} {order[1]} {order[2]}\n'
-            f.write(header_3)
-
-            n = 0
-            for i in range(coefs_3.shape[0]):
-                for j in range(coefs_3.shape[1]):
-                    for k in range(coefs_3.shape[2]):
-                        coef = coefs_3[i, j, k]
-                        f.write(f'{coef:.10e} ')
-                        if n % 5 == 4:
-                            f.write('\n')
-                        n += 1
-
-            f.write('\n')
-
     def write_lmp_file(self, lammps_name):
         '''
         write the coefficients to a file that can be used by lammps pair style
@@ -416,18 +361,21 @@ class MappedGaussianProcess:
 
         twobodyarray = len(self.spcs[0])
         threebodyarray = len(self.spcs[1])
-        header = f'\n{twobodyarray} {threebodyarray}\n'
+        header = '\n{} {}\n'.format(twobodyarray, threebodyarray)
         f.write(header)
 
         # write two body
         if twobodyarray > 0:
-            self.write_two_body(f)
+            for ind, spc in enumerate(self.spcs[0]):
+                self.maps_2[ind].write(f, spc)
 
         # write three body
         if threebodyarray > 0:
-            self.write_three_body(f)
+            for ind, spc in enumerate(self.spcs[1]):
+                self.maps_3[ind].write(f, spc)
 
         f.close()
+
 
     def write_model(self, model_name):
         """
@@ -565,6 +513,29 @@ class Map2body:
         self.mean.set_values(y_mean)
         if not self.mean_only:
             self.var.set_values(y_var)
+
+    def write(self, f, spc):
+        '''
+        Write LAMMPS coefficient file
+        '''
+        a = self.l_bounds[0]
+        b = self.u_bounds[0]
+        order = self.grid_num
+
+        coefs_2 = self.mean.__coeffs__
+
+        elem1 = Z_to_element(spc)
+        elem2 = Z_to_element(spc)
+        header_2 = '{elem1} {elem2} {a} {b} {order}\n'\
+            .format(elem1=elem1, elem2=elem2, a=a, b=b, order=order)
+        f.write(header_2)
+
+        for c, coef in enumerate(coefs_2):
+            f.write('{:.10e} '.format(coef))
+            if c % 5 == 4 and c != len(coefs_2)-1:
+                f.write('\n')
+
+        f.write('\n')
 
 
 
@@ -745,4 +716,36 @@ class Map3body:
         self.mean.set_values(y_mean)
         if not self.mean_only:
             self.var.set_values(y_var)
+
+    def write(self, f, spc):
+        a = self.l_bounds
+        b = self.u_bounds
+        order = self.grid_num
+
+        coefs_3 = self.mean.__coeffs__
+
+        elem1 = Z_to_element(spc[0])
+        elem2 = Z_to_element(spc[1])
+        elem3 = Z_to_element(spc[2])
+
+        header_3 = '{elem1} {elem2} {elem3} {a1} {a2} {a3} {b1}'\
+                   ' {b2} {b3:.10e} {order1} {order2} {order3}\n'\
+            .format(elem1=elem1, elem2=elem2, elem3=elem3,
+                    a1=a[0], a2=a[1], a3=a[2],
+                    b1=b[0], b2=b[1], b3=b[2],
+                    order1=order[0], order2=order[1], order3=order[2])
+        f.write(header_3)
+
+        n = 0
+        for i in range(coefs_3.shape[0]):
+            for j in range(coefs_3.shape[1]):
+                for k in range(coefs_3.shape[2]):
+                    coef = coefs_3[i, j, k]
+                    f.write('{:.10e} '.format(coef))
+                    if n % 5 == 4:
+                        f.write('\n')
+                    n += 1
+
+        f.write('\n')
+
 
