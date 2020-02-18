c7e3b5fbe25a512d243857da1aa93f90c9b70054
Claudio Zeni <claudiopuntozeni@gmail.com>
2020-02-18 20:11:15 +0000

    added force energy many body test

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -583,7 +583,6 @@ def test_many_body_force():
     """Check that the analytical force kernel matches finite difference of
     energy kernel."""
 
-    # TODO: why env1_1_1, env1_1_2 and env1_1_3 (and other variables) are never used?
     # create env 1
     delt = 1e-5
     cell = 10.0 * np.eye(3)
@@ -606,19 +605,14 @@ def test_many_body_force():
     test_structure_3 = struc.Structure(cell, species_1, positions_3)
 
     env1_1_0 = env.AtomicEnvironment(test_structure_1, 0, cutoffs)
-    env1_1_1 = env.AtomicEnvironment(test_structure_1, 1, cutoffs)
-    env1_1_2 = env.AtomicEnvironment(test_structure_1, 2, cutoffs)
-    env1_1_3 = env.AtomicEnvironment(test_structure_1, 3, cutoffs)
 
     env1_2_0 = env.AtomicEnvironment(test_structure_2, 0, cutoffs)
     env1_2_1 = env.AtomicEnvironment(test_structure_2, 1, cutoffs)
     env1_2_2 = env.AtomicEnvironment(test_structure_2, 2, cutoffs)
-    env1_2_3 = env.AtomicEnvironment(test_structure_2, 3, cutoffs)
 
     env1_3_0 = env.AtomicEnvironment(test_structure_3, 0, cutoffs)
     env1_3_1 = env.AtomicEnvironment(test_structure_3, 1, cutoffs)
     env1_3_2 = env.AtomicEnvironment(test_structure_3, 2, cutoffs)
-    env1_3_3 = env.AtomicEnvironment(test_structure_3, 3, cutoffs)
 
     # create env 2
     positions_1 = [np.array([0., 0., 0.]),
@@ -632,25 +626,19 @@ def test_many_body_force():
     positions_3[0][1] = -delt
 
     species_2 = [1, 1, 1, 1]
-    atom_2 = 0
     test_structure_1 = struc.Structure(cell, species_2, positions_1)
     test_structure_2 = struc.Structure(cell, species_2, positions_2)
     test_structure_3 = struc.Structure(cell, species_2, positions_3)
 
     env2_1_0 = env.AtomicEnvironment(test_structure_1, 0, cutoffs)
-    env2_1_1 = env.AtomicEnvironment(test_structure_1, 1, cutoffs)
-    env2_1_2 = env.AtomicEnvironment(test_structure_1, 2, cutoffs)
-    env2_1_3 = env.AtomicEnvironment(test_structure_1, 3, cutoffs)
 
     env2_2_0 = env.AtomicEnvironment(test_structure_2, 0, cutoffs)
     env2_2_1 = env.AtomicEnvironment(test_structure_2, 1, cutoffs)
     env2_2_2 = env.AtomicEnvironment(test_structure_2, 2, cutoffs)
-    env2_2_3 = env.AtomicEnvironment(test_structure_2, 3, cutoffs)
 
     env2_3_0 = env.AtomicEnvironment(test_structure_3, 0, cutoffs)
     env2_3_1 = env.AtomicEnvironment(test_structure_3, 1, cutoffs)
     env2_3_2 = env.AtomicEnvironment(test_structure_3, 2, cutoffs)
-    env2_3_3 = env.AtomicEnvironment(test_structure_3, 3, cutoffs)
 
     sig = 1
     ls = 0.1
@@ -743,6 +731,84 @@ def test_many_body_force():
 
     assert (np.isclose(kern_finite_diff, kern_analytical, atol=tol))
 
+
+def test_many_body_force_en():
+    """Check that the analytical force-energy kernel matches finite difference of
+    energy kernel."""
+
+    # TODO: why env1_1_1, env1_1_2 and env1_1_3 (and other variables) are never used?
+    # create env 1
+    delt = 1e-5
+    cell = 10.0 * np.eye(3)
+    cutoffs = np.array([1.2, 1.2, 1.2])
+
+    positions_1 = [np.array([0., 0., 0.]),
+                   np.array([0., 1., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 0., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 1., 0.]) + 0.1 * np.array([random(), random(), random()])]
+
+    positions_2 = deepcopy(positions_1)
+    positions_2[0][0] = delt
+
+    positions_3 = deepcopy(positions_1)
+    positions_3[0][0] = -delt
+
+    species_1 = [1, 1, 1, 1]
+
+    test_structure_1 = struc.Structure(cell, species_1, positions_1)
+    test_structure_2 = struc.Structure(cell, species_1, positions_2)
+    test_structure_3 = struc.Structure(cell, species_1, positions_3)
+
+    env1_1_0 = env.AtomicEnvironment(test_structure_1, 0, cutoffs)
+
+    env1_2_0 = env.AtomicEnvironment(test_structure_2, 0, cutoffs)
+    env1_2_1 = env.AtomicEnvironment(test_structure_2, 1, cutoffs)
+    env1_2_2 = env.AtomicEnvironment(test_structure_2, 2, cutoffs)
+
+    env1_3_0 = env.AtomicEnvironment(test_structure_3, 0, cutoffs)
+    env1_3_1 = env.AtomicEnvironment(test_structure_3, 1, cutoffs)
+    env1_3_2 = env.AtomicEnvironment(test_structure_3, 2, cutoffs)
+
+    # create env 2
+    positions_1 = [np.array([0., 0., 0.]),
+                   np.array([0., 1., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 0., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 1., 0.]) + 0.1 * np.array([random(), random(), random()])]
+
+    species_2 = [1, 1, 1, 1]
+
+    test_structure_1 = struc.Structure(cell, species_2, positions_1)
+
+    env2_1_0 = env.AtomicEnvironment(test_structure_1, 0, cutoffs)
+
+    sig = 1
+    ls = 0.1
+    r0 = 1
+    d1 = 1
+
+    hyps = np.array([sig, ls, r0])
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_0, env2_1_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_0, env2_1_0, hyps, cutoffs)
+    kern_finite_diff_00 = (calc1 - calc2) / (2 * delt)
+
+    calc1 = en.many_body_en(env1_2_1, env2_1_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_1, env2_1_0, hyps, cutoffs)
+    kern_finite_diff_10 = (calc1 - calc2) / (2 * delt)
+
+    calc1 = en.many_body_en(env1_2_2, env2_1_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_2, env2_1_0, hyps, cutoffs)
+    kern_finite_diff_20 = (calc1 - calc2) / (2 * delt)
+
+    kern_finite_diff = -(kern_finite_diff_00 + kern_finite_diff_10 + kern_finite_diff_20)
+
+    kern_analytical = en.many_body_force_en(env1_1_0, env2_1_0,
+                                   d1, hyps, cutoffs)
+
+    tol = 1e-4
+
+    assert (np.isclose(kern_finite_diff, kern_analytical, atol=tol))    
 
 def test_many_body_grad():
     # create env 1
