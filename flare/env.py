ca4ec88b6229e958a058ea8cf796e4aa7ea2aa45
Claudio Zeni <claudiopuntozeni@gmail.com>
2020-02-19 21:49:48 +0000

    Updated env to take into account species of neighbours of neighbours

--- flare/env.py
+++ flare/env.py
@@ -53,7 +53,7 @@ class AtomicEnvironment:
         # if 3 cutoffs are given, create many-body arrays
         if len(cutoffs) > 2:
             self.cutoff_mb = cutoffs[2]
-            self.bond_array_mb, self.neigh_dists_mb, self.num_neighs_mb = get_m_body_arrays(
+            self.bond_array_mb, self.neigh_dists_mb, self.num_neighs_mb, self.etype_mb = get_m_body_arrays(
                 self.positions, self.atom, self.cell, self.cutoff_mb, self.species)
 
     def as_dict(self):
@@ -405,22 +405,27 @@ def get_m_body_arrays(positions, atom: int, cell, cutoff_mb: float, species):
 
     # For each neighbouring atom, get distances in its neighbourhood
     neighbouring_dists = []
+    neighbouring_etypes = []
     max_neighbours = 0
     for m in bond_inds:
-        neighbour_bond_array_2, __, _ = get_2_body_arrays(positions, m, cell,
+        neighbour_bond_array_2, ___, etypes_mb = get_2_body_arrays(positions, m, cell,
                                                          cutoff_mb, species)
         neighbouring_dists.append(neighbour_bond_array_2[:, 0])
+        neighbouring_etypes.append(etypes_mb)
         if len(neighbour_bond_array_2[:, 0]) > max_neighbours:
             max_neighbours = len(neighbour_bond_array_2[:, 0])
 
     # Transform list of distances into Numpy array
     neigh_dists_mb = np.zeros((len(bond_inds), max_neighbours))
     num_neighs_mb = np.zeros(len(bond_inds), dtype=np.int8)
+    etypes_mb_array = np.zeros((len(bond_inds), max_neighbours), dtype=np.int8)
     for i in range(len(bond_inds)):
         num_neighs_mb[i] = len(neighbouring_dists[i])
         neigh_dists_mb[i, :num_neighs_mb[i]] = neighbouring_dists[i]
+        etypes_mb_array[i, :num_neighs_mb[i]] = etypes_mb[i]
 
-    return bond_array_mb, neigh_dists_mb, num_neighs_mb
+
+    return bond_array_mb, neigh_dists_mb, num_neighs_mb, etypes_mb_array
 
 
 if __name__ == '__main__':
