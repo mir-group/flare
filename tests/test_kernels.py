fa3f572c56b560488871f16cda3418a023773582
Claudio Zeni <claudiopuntozeni@gmail.com>
2020-02-21 17:11:54 +0000

    Merged many-body single component kernel tests

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -664,3 +664,293 @@ def test_grad_mask_function():
     grad = [1, 2, 3, 4, 5]
     hyps_mask = {'map': [0, 3, 5]}
     assert (from_grad_to_mask(grad, hyps_mask) == [1, 4]).all()
+
+
+# -----------------------------------------------------------------------------
+#                              test many body kernels
+# -----------------------------------------------------------------------------
+
+
+def test_many_body_force():
+    """Check that the analytical force kernel matches finite difference of
+    energy kernel."""
+
+    # create env 1
+    delt = 1e-5
+    cell = 10.0 * np.eye(3)
+    cutoffs = np.array([1.2, 1.2, 1.2])
+
+    positions_1 = [np.array([0., 0., 0.]),
+                   np.array([0., 1., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 0., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 1., 0.]) + 0.1 * np.array([random(), random(), random()])]
+
+    positions_2 = deepcopy(positions_1)
+    positions_2[0][0] = delt
+
+    positions_3 = deepcopy(positions_1)
+    positions_3[0][0] = -delt
+
+    species_1 = [1, 1, 1, 1]
+    test_structure_1 = struc.Structure(cell, species_1, positions_1)
+    test_structure_2 = struc.Structure(cell, species_1, positions_2)
+    test_structure_3 = struc.Structure(cell, species_1, positions_3)
+
+    env1_1_0 = env.AtomicEnvironment(test_structure_1, 0, cutoffs)
+
+    env1_2_0 = env.AtomicEnvironment(test_structure_2, 0, cutoffs)
+    env1_2_1 = env.AtomicEnvironment(test_structure_2, 1, cutoffs)
+    env1_2_2 = env.AtomicEnvironment(test_structure_2, 2, cutoffs)
+
+    env1_3_0 = env.AtomicEnvironment(test_structure_3, 0, cutoffs)
+    env1_3_1 = env.AtomicEnvironment(test_structure_3, 1, cutoffs)
+    env1_3_2 = env.AtomicEnvironment(test_structure_3, 2, cutoffs)
+
+    # create env 2
+    positions_1 = [np.array([0., 0., 0.]),
+                   np.array([0., 1., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 0., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 1., 0.]) + 0.1 * np.array([random(), random(), random()])]
+
+    positions_2 = deepcopy(positions_1)
+    positions_2[0][1] = delt
+    positions_3 = deepcopy(positions_1)
+    positions_3[0][1] = -delt
+
+    species_2 = [1, 1, 1, 1]
+    test_structure_1 = struc.Structure(cell, species_2, positions_1)
+    test_structure_2 = struc.Structure(cell, species_2, positions_2)
+    test_structure_3 = struc.Structure(cell, species_2, positions_3)
+
+    env2_1_0 = env.AtomicEnvironment(test_structure_1, 0, cutoffs)
+
+    env2_2_0 = env.AtomicEnvironment(test_structure_2, 0, cutoffs)
+    env2_2_1 = env.AtomicEnvironment(test_structure_2, 1, cutoffs)
+    env2_2_2 = env.AtomicEnvironment(test_structure_2, 2, cutoffs)
+
+    env2_3_0 = env.AtomicEnvironment(test_structure_3, 0, cutoffs)
+    env2_3_1 = env.AtomicEnvironment(test_structure_3, 1, cutoffs)
+    env2_3_2 = env.AtomicEnvironment(test_structure_3, 2, cutoffs)
+
+    sig = random()
+    ls = random()
+    d1 = 1
+    d2 = 2
+
+    hyps = np.array([sig, ls])
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_0, env2_2_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_0, env2_3_0, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_0, env2_3_0, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_0, env2_2_0, hyps, cutoffs)
+
+    kern_finite_diff_00 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_0, env2_2_1, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_0, env2_3_1, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_0, env2_3_1, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_0, env2_2_1, hyps, cutoffs)
+
+    kern_finite_diff_01 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_0, env2_2_2, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_0, env2_3_2, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_0, env2_3_2, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_0, env2_2_2, hyps, cutoffs)
+
+    kern_finite_diff_02 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_1, env2_2_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_1, env2_3_0, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_1, env2_3_0, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_1, env2_2_0, hyps, cutoffs)
+
+    kern_finite_diff_10 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_1, env2_2_1, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_1, env2_3_1, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_1, env2_3_1, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_1, env2_2_1, hyps, cutoffs)
+
+    kern_finite_diff_11 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_1, env2_2_2, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_1, env2_3_2, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_1, env2_3_2, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_1, env2_2_2, hyps, cutoffs)
+
+    kern_finite_diff_12 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_2, env2_2_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_2, env2_3_0, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_2, env2_3_0, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_2, env2_2_0, hyps, cutoffs)
+
+    kern_finite_diff_20 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_2, env2_2_1, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_2, env2_3_1, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_2, env2_3_1, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_2, env2_2_1, hyps, cutoffs)
+
+    kern_finite_diff_21 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_2, env2_2_2, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_2, env2_3_2, hyps, cutoffs)
+    calc3 = en.many_body_en(env1_2_2, env2_3_2, hyps, cutoffs)
+    calc4 = en.many_body_en(env1_3_2, env2_2_2, hyps, cutoffs)
+
+    kern_finite_diff_22 = (calc1 + calc2 - calc3 - calc4) / (4 * delt ** 2)
+
+    kern_finite_diff = (kern_finite_diff_00 + kern_finite_diff_01 + kern_finite_diff_02 +
+                        kern_finite_diff_10 + kern_finite_diff_11 + kern_finite_diff_12 +
+                        kern_finite_diff_20 + kern_finite_diff_21 + kern_finite_diff_22)
+
+    kern_analytical = en.many_body(env1_1_0, env2_1_0,
+                                   d1, d2, hyps, cutoffs)
+
+    tol = 1e-4
+
+    assert (np.isclose(kern_finite_diff, kern_analytical, atol=tol))
+
+
+def test_many_body_force_en():
+    """Check that the analytical force-energy kernel matches finite difference of
+    energy kernel."""
+
+    # TODO: why env1_1_1, env1_1_2 and env1_1_3 (and other variables) are never used?
+    # create env 1
+    delt = 1e-5
+    cell = 10.0 * np.eye(3)
+    cutoffs = np.array([1.2, 1.2, 1.2])
+
+    positions_1 = [np.array([0., 0., 0.]),
+                   np.array([0., 1., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 0., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 1., 0.]) + 0.1 * np.array([random(), random(), random()])]
+
+    positions_2 = deepcopy(positions_1)
+    positions_2[0][0] = delt
+
+    positions_3 = deepcopy(positions_1)
+    positions_3[0][0] = -delt
+
+    species_1 = [1, 1, 1, 1]
+
+    test_structure_1 = struc.Structure(cell, species_1, positions_1)
+    test_structure_2 = struc.Structure(cell, species_1, positions_2)
+    test_structure_3 = struc.Structure(cell, species_1, positions_3)
+
+    env1_1_0 = env.AtomicEnvironment(test_structure_1, 0, cutoffs)
+
+    env1_2_0 = env.AtomicEnvironment(test_structure_2, 0, cutoffs)
+    env1_2_1 = env.AtomicEnvironment(test_structure_2, 1, cutoffs)
+    env1_2_2 = env.AtomicEnvironment(test_structure_2, 2, cutoffs)
+
+    env1_3_0 = env.AtomicEnvironment(test_structure_3, 0, cutoffs)
+    env1_3_1 = env.AtomicEnvironment(test_structure_3, 1, cutoffs)
+    env1_3_2 = env.AtomicEnvironment(test_structure_3, 2, cutoffs)
+
+    # create env 2
+    positions_1 = [np.array([0., 0., 0.]),
+                   np.array([0., 1., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 0., 0.]) + 0.1 * np.array([random(), random(), random()]),
+                   np.array([1., 1., 0.]) + 0.1 * np.array([random(), random(), random()])]
+
+    species_2 = [1, 1, 1, 1]
+
+    test_structure_1 = struc.Structure(cell, species_2, positions_1)
+
+    env2_1_0 = env.AtomicEnvironment(test_structure_1, 0, cutoffs)
+
+    sig = random()
+    ls = random()
+    d1 = 1
+
+    hyps = np.array([sig, ls])
+
+    # check force kernel
+    calc1 = en.many_body_en(env1_2_0, env2_1_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_0, env2_1_0, hyps, cutoffs)
+    kern_finite_diff_00 = (calc1 - calc2) / (2 * delt)
+
+    calc1 = en.many_body_en(env1_2_1, env2_1_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_1, env2_1_0, hyps, cutoffs)
+    kern_finite_diff_10 = (calc1 - calc2) / (2 * delt)
+
+    calc1 = en.many_body_en(env1_2_2, env2_1_0, hyps, cutoffs)
+    calc2 = en.many_body_en(env1_3_2, env2_1_0, hyps, cutoffs)
+    kern_finite_diff_20 = (calc1 - calc2) / (2 * delt)
+
+    kern_finite_diff = -(kern_finite_diff_00 + kern_finite_diff_10 + kern_finite_diff_20)
+
+    kern_analytical = en.many_body_force_en(env1_1_0, env2_1_0,
+                                            d1, hyps, cutoffs)
+
+    tol = 1e-4
+
+    assert (np.isclose(kern_finite_diff, kern_analytical, atol=tol))
+
+
+def test_many_body_grad():
+    # create env 1
+    cell = np.eye(3)
+    cutoffs = np.array([1, 1, 1])
+
+    positions_1 = [np.array([0., 0., 0.]),
+                   np.array([random(), random(), random()]),
+                   np.array([random(), random(), random()])]
+
+    species_1 = [1, 1, 1]
+    atom_1 = 0
+    test_structure_1 = struc.Structure(cell, species_1, positions_1)
+    env1 = env.AtomicEnvironment(test_structure_1, atom_1, cutoffs)
+
+    # create env 2
+    positions_1 = [np.array([0., 0., 0.]),
+                   np.array([random(), random(), random()]),
+                   np.array([random(), random(), random()])]
+
+    species_2 = [1, 1, 1]
+    atom_2 = 0
+    test_structure_1 = struc.Structure(cell, species_2, positions_1)
+    env2 = env.AtomicEnvironment(test_structure_1, atom_2, cutoffs)
+
+    sig = random()
+    ls = random()
+
+    d1 = randint(1, 3)
+    d2 = randint(1, 3)
+
+    hyps = np.array([sig, ls])
+
+    grad_test = en.many_body_grad(env1, env2, d1, d2, hyps, cutoffs)
+
+    delta = 1e-8
+    new_sig = sig + delta
+    new_ls = ls + delta
+
+    sig_derv_brute = (en.many_body(env1, env2, d1, d2,
+                                   np.array([new_sig, ls]),
+                                   cutoffs) -
+                      en.many_body(env1, env2, d1, d2,
+                                   hyps, cutoffs)) / delta
+
+    l_derv_brute = (en.many_body(env1, env2, d1, d2,
+                                 np.array([sig, new_ls]),
+                                 cutoffs) -
+                    en.many_body(env1, env2, d1, d2,
+                                 hyps, cutoffs)) / delta
+
+    tol = 1e-4
+    assert (np.isclose(grad_test[1][0], sig_derv_brute, atol=tol))
+    assert (np.isclose(grad_test[1][1], l_derv_brute, atol=tol))
