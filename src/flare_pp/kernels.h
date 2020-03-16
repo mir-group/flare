22e211b0ee031626ad443c02992218546e5019d1
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-03-16 16:31:28 -0400

    add 3-body self kernel

--- src/flare_pp/kernels.h
+++ src/flare_pp/kernels.h
@@ -100,14 +100,79 @@ class ThreeBodyKernel : public Kernel{
             double yrel1, double yval1, double yrel2, double yval2,
             double zrel1, double zval1, double zrel2, double zval2);
 
-        void env_env_update(Eigen::VectorXd & kernel_vector,
+        void env_env_update_1(Eigen::VectorXd & kernel_vector,
             int no_elements, int i, double vol_inv,
             double r11, double r22, double r33,
-            double fi, double fj, double fdjx1, double fdjx2,
-            double fdjy1, double fdjy2, double fdjz1, double fdjz2,
-            double xrel1, double xval1, double xrel2, double xval2,
-            double yrel1, double yval1, double yrel2, double yval2,
-            double zrel1, double zval1, double zrel2, double zval2);
+            double fi, double fj,
+            double fdix1, double fdix2, double fdjx1, double fdjx2,
+            double fdiy1, double fdiy2, double fdjy1, double fdjy2,
+            double fdiz1, double fdiz2, double fdjz1, double fdjz2,
+            double xrel_i1, double xval_i1, double xrel_i2, double xval_i2,
+            double yrel_i1, double yval_i1, double yrel_i2, double yval_i2,
+            double zrel_i1, double zval_i1, double zrel_i2, double zval_i2,
+            double xrel_j1, double xval_j1, double xrel_j2, double xval_j2,
+            double yrel_j1, double yval_j1, double yrel_j2, double yval_j2,
+            double zrel_j1, double zval_j1, double zrel_j2, double zval_j2);
+        
+        void env_env_update_2(Eigen::VectorXd & kernel_vector,
+            int no_elements, int i, double vol_inv,
+            double r11, double r22, double r33,
+            double fi, double fj,
+            double fdix1, double fdix2, double fdjx1, double fdjx2,
+            double fdiy1, double fdiy2, double fdjy1, double fdjy2,
+            double fdiz1, double fdiz2, double fdjz1, double fdjz2,
+            double xrel_i1, double xval_i1, double xrel_i2, double xval_i2,
+            double yrel_i1, double yval_i1, double yrel_i2, double yval_i2,
+            double zrel_i1, double zval_i1, double zrel_i2, double zval_i2,
+            double xrel_j1, double xval_j1, double xrel_j2, double xval_j2,
+            double yrel_j1, double yval_j1, double yrel_j2, double yval_j2,
+            double zrel_j1, double zval_j1, double zrel_j2, double zval_j2);
+
+        void struc_struc_update_1(Eigen::VectorXd & kernel_vector,
+            int no_elements, int i, int j, double mult_factor, double vol_inv,
+            double r11, double r22, double r33,
+            double fi, double fj,
+            double fdix1, double fdix2, double fdjx1, double fdjx2,
+            double fdiy1, double fdiy2, double fdjy1, double fdjy2,
+            double fdiz1, double fdiz2, double fdjz1, double fdjz2,
+            double xrel_i1, double xval_i1, double xrel_i2, double xval_i2,
+            double yrel_i1, double yval_i1, double yrel_i2, double yval_i2,
+            double zrel_i1, double zval_i1, double zrel_i2, double zval_i2,
+            double xrel_j1, double xval_j1, double xrel_j2, double xval_j2,
+            double yrel_j1, double yval_j1, double yrel_j2, double yval_j2,
+            double zrel_j1, double zval_j1, double zrel_j2, double zval_j2);
+        
+        std::vector<double> force_stress_helper_1(
+            double mult_factor, double vol_inv_sq, double k_SE,
+            double r11, double r22, double r33, double fi, double fj,
+            double fdix1, double fdix2, double fdjx1, double fdjx2,
+            double xrel_i1, double xrel_i2, double xrel_j1, double xrel_j2, 
+            double xval_i1, double xval_i2, double yval_i1, double yval_i2,
+            double zval_i1, double zval_i2, double xval_j1, double xval_j2,
+            double yval_j1, double yval_j2, double zval_j1, double zval_j2);
+
+        void struc_struc_update_2(Eigen::VectorXd & kernel_vector,
+            int no_elements, int i, int j, double mult_factor, double vol_inv,
+            double r12, double r23, double r31,
+            double fi, double fj,
+            double fdix1, double fdix2, double fdjx1, double fdjx2,
+            double fdiy1, double fdiy2, double fdjy1, double fdjy2,
+            double fdiz1, double fdiz2, double fdjz1, double fdjz2,
+            double xrel_i1, double xval_i1, double xrel_i2, double xval_i2,
+            double yrel_i1, double yval_i1, double yrel_i2, double yval_i2,
+            double zrel_i1, double zval_i1, double zrel_i2, double zval_i2,
+            double xrel_j1, double xval_j1, double xrel_j2, double xval_j2,
+            double yrel_j1, double yval_j1, double yrel_j2, double yval_j2,
+            double zrel_j1, double zval_j1, double zrel_j2, double zval_j2);
+
+        std::vector<double> force_stress_helper_2(
+            double mult_factor, double vol_inv_sq, double k_SE,
+            double r12, double r23, double r31, double fi, double fj,
+            double fdix1, double fdix2, double fdjx1, double fdjx2,
+            double xrel_i1, double xrel_i2, double xrel_j1, double xrel_j2, 
+            double xval_i1, double xval_i2, double yval_i1, double yval_i2,
+            double zval_i1, double zval_i2, double xval_j1, double xval_j2,
+            double yval_j1, double yval_j2, double zval_j1, double zval_j2);
 };
 
 class DotProductKernel : public Kernel{
