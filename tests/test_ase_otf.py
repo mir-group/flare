b408b96ed497d3e508872779edd984e6ddc9ff3f
Yu Xie <xieyu.fish@gmail.com>
2020-05-11 12:10:52 -0400

    change mgp_en to mgp

--- tests/test_ase_otf.py
+++ tests/test_ase_otf.py
@@ -73,17 +73,17 @@ def flare_calc():
                        'svd_rank_3': 0,
                        'bodies': [2, 3],
                        'load_grid': None,
-                       'update': True}
+                       'update': False}
 
         mgp_model = MappedGaussianProcess(grid_params,
                                           struc_params,
-                                          map_force=False,
+                                          map_force=True,
                                           GP=gp_model,
                                           mean_only=False,
                                           container_only=False,
                                           lmp_file_name='lmp.mgp',
                                           n_cpus=1,
-                                          autorun=False)
+                                          autorun=True)
 
         # ------------ create ASE's flare calculator -----------------------
         flare_calculator = FLARE_Calculator(gp_model, mgp_model,
