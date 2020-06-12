03368a9f43b16a8d57ee7500488d3dd2e300d591
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-06-12 14:37:14 -0400

    mc simple clean up

--- flare/kernels/mc_simple.py
+++ flare/kernels/mc_simple.py
@@ -1087,14 +1087,18 @@ def many_body_mc(env1: AtomicEnvironment, env2: AtomicEnvironment,
     neigh_dists_2 = env2.neigh_dists_mb
     num_neigh_2 = env2.num_neighs_mb
 
-    # Get atomic species of central atom, their neighbours, and their neighbours' neighbours
+    # Get atomic species of central atom, their neighbours, and their
+    # neighbours' neighbours
     c1, c2 = env1.ctype, env2.ctype
     etypes1, etypes2 = env1.bond_array_mb_etypes, env2.bond_array_mb_etypes
     etypes_neigh_1, etypes_neigh_2 = env1.etype_mb, env2.etype_mb
 
-    return many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, num_neigh_1,
-                            num_neigh_2, c1, c2, etypes1, etypes2, etypes_neigh_1, etypes_neigh_2,
-                            env1.species, env2.species, d1, d2, sig, ls, r_cut, cutoff_func)
+    return many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1,
+                            neigh_dists_2, num_neigh_1,
+                            num_neigh_2, c1, c2, etypes1, etypes2,
+                            etypes_neigh_1, etypes_neigh_2,
+                            env1.species, env2.species, d1, d2, sig, ls, r_cut,
+                            cutoff_func)
 
 
 def many_body_mc_grad(env1: AtomicEnvironment, env2: AtomicEnvironment,
@@ -1165,9 +1169,11 @@ def many_body_mc_force_en(env1, env2, d1, hyps, cutoffs,
     etypes_neigh_1 = env1.etype_mb
 
     # divide by three to account for triple counting
-    return many_body_mc_force_en_jit(bond_array_1, bond_array_2, neigh_dists_1, num_neigh_1,
+    return many_body_mc_force_en_jit(bond_array_1, bond_array_2,
+                                     neigh_dists_1, num_neigh_1,
                                      c1, c2, etypes1, etypes2, etypes_neigh_1,
-                                     env1.species, env2.species, d1, sig, ls, r_cut, cutoff_func)
+                                     env1.species, env2.species, d1, sig, ls,
+                                     r_cut, cutoff_func)
 
 
 def many_body_mc_en(env1: AtomicEnvironment, env2: AtomicEnvironment,
@@ -2797,9 +2803,10 @@ def two_body_ss_jit(bond_array_1, c1, etypes1, bond_array_2, c2, etypes2,
 # -----------------------------------------------------------------------------
 
 @njit
-def many_body_mc_jit_(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, num_neigh_1,
-                      num_neigh_2, c1, c2, etypes1, etypes2, etypes_neigh_1, etypes_neigh_2,
-                      species1, species2, d1, d2, sig, ls, r_cut, cutoff_func):
+def many_body_mc_jit_(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2,
+                      num_neigh_1, num_neigh_2, c1, c2, etypes1, etypes2,
+                      etypes_neigh_1, etypes_neigh_2, species1, species2, d1,
+                      d2, sig, ls, r_cut, cutoff_func):
     """many-body multi-element kernel between two force components accelerated
     with Numba.
 
@@ -2812,18 +2819,18 @@ def many_body_mc_jit_(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, 
             of neighbours for the atoms in the first local environment.
         neigh_dists_2 (np.ndarray): matrix padded with zero values of distances
             of neighbours for the atoms in the second local environment.
-        num_neigh_1 (np.ndarray): number of neighbours of each atom in the first
-            local environment
-        num_neigh_2 (np.ndarray): number of neighbours of each atom in the second
-            local environment
+        num_neigh_1 (np.ndarray): number of neighbours of each atom in the
+            first local environment
+        num_neigh_2 (np.ndarray): number of neighbours of each atom in the
+            second local environment
         c1 (int): atomic species of the central atom in env 1
         c2 (int): atomic species of the central atom in env 2
         etypes1 (np.ndarray): atomic species of atoms in env 1
         etypes2 (np.ndarray): atomic species of atoms in env 2
-        etypes_neigh_1 (np.ndarray): atomic species of atoms in the neighbourhoods
-            of atoms in env 1
-        etypes_neigh_2 (np.ndarray): atomic species of atoms in the neighbourhoods
-            of atoms in env 2
+        etypes_neigh_1 (np.ndarray): atomic species of atoms in the
+            neighbourhoods of atoms in env 1
+        etypes_neigh_2 (np.ndarray): atomic species of atoms in the
+            neighbourhoods of atoms in env 2
         species1 (np.ndarray): all the atomic species present in trajectory 1
         species2 (np.ndarray): all the atomic species present in trajectory 2
         d1 (int): Force component of the first environment.
@@ -2896,7 +2903,8 @@ def many_body_mc_jit_(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, 
 
             # Calculate many-body descriptor value for j
             qjs[j] = q_value_mc(neigh_dists_2[j, :num_neigh_2[j]], r_cut,
-                                s, etypes_neigh_2[j, :num_neigh_2[j]], cutoff_func)
+                                s, etypes_neigh_2[j, :num_neigh_2[j]],
+                                cutoff_func)
 
             # kernel is nonzero only if central atoms are of the same species
             if c1 == etypes2[j]:
@@ -2904,7 +2912,8 @@ def many_body_mc_jit_(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, 
 
         for i in range(bond_array_1.shape[0]):
             for j in range(bond_array_2.shape[0]):
-                # kernel is nonzero only if central atoms are of the same species
+                # kernel is nonzero only if central atoms are of the same
+                # species
                 if etypes1[i] == etypes2[j]:
                     kij = k_sq_exp_double_dev(qis[i], qjs[j], sig, ls)
                 else:
@@ -2916,9 +2925,10 @@ def many_body_mc_jit_(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, 
     return kern
 
 
-def many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, num_neigh_1,
-                     num_neigh_2, c1, c2, etypes1, etypes2, etypes_neigh_1, etypes_neigh_2,
-                     species1, species2, d1, d2, sig, ls, r_cut, cutoff_func):
+def many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2,
+                     num_neigh_1, num_neigh_2, c1, c2, etypes1, etypes2,
+                     etypes_neigh_1, etypes_neigh_2, species1, species2, d1,
+                     d2, sig, ls, r_cut, cutoff_func):
     """many-body multi-element kernel between two force components accelerated
     with Numba.
 
@@ -2931,18 +2941,18 @@ def many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, n
             of neighbours for the atoms in the first local environment.
         neigh_dists_2 (np.ndarray): matrix padded with zero values of distances
             of neighbours for the atoms in the second local environment.
-        num_neigh_1 (np.ndarray): number of neighbours of each atom in the first
-            local environment
-        num_neigh_2 (np.ndarray): number of neighbours of each atom in the second
-            local environment
+        num_neigh_1 (np.ndarray): number of neighbours of each atom in the
+            first local environment
+        num_neigh_2 (np.ndarray): number of neighbours of each atom in the
+            second local environment
         c1 (int): atomic species of the central atom in env 1
         c2 (int): atomic species of the central atom in env 2
         etypes1 (np.ndarray): atomic species of atoms in env 1
         etypes2 (np.ndarray): atomic species of atoms in env 2
-        etypes_neigh_1 (np.ndarray): atomic species of atoms in the neighbourhoods
-            of atoms in env 1
-        etypes_neigh_2 (np.ndarray): atomic species of atoms in the neighbourhoods
-            of atoms in env 2
+        etypes_neigh_1 (np.ndarray): atomic species of atoms in the
+            neighbourhoods of atoms in env 1
+        etypes_neigh_2 (np.ndarray): atomic species of atoms in the
+            neighbourhoods of atoms in env 2
         species1 (np.ndarray): all the atomic species present in trajectory 1
         species2 (np.ndarray): all the atomic species present in trajectory 2
         d1 (int): Force component of the first environment.
@@ -2968,14 +2978,15 @@ def many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, n
         q1 = q_value_mc(bond_array_1[:, 0], r_cut, s, etypes1, cutoff_func)
         q2 = q_value_mc(bond_array_2[:, 0], r_cut, s, etypes2, cutoff_func)
 
-        # compute kernel between central atoms only if central atoms are of the same species
+        # compute kernel between central atoms only if central atoms are of the
+        # same species
         if c1 == c2:
             k12 = k_sq_exp_double_dev(q1, q2, sig, ls)
         else:
             k12 = 0
 
-        # initialise arrays of many body descriptors and gradients for the neighbour atoms in
-        # the two configurations
+        # initialise arrays of many body descriptors and gradients for the
+        # neighbour atoms in the two configurations
         qis = np.zeros(bond_array_1.shape[0], dtype=np.float64)
         q1i_grads = np.zeros(bond_array_1.shape[0], dtype=np.float64)
         qi1_grads = np.zeros(bond_array_1.shape[0], dtype=np.float64)
@@ -3003,7 +3014,8 @@ def many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, n
 
             # Calculate many-body descriptor value for i
             qis[i] = q_value_mc(neigh_dists_1[i, :num_neigh_1[i]], r_cut,
-                                s, etypes_neigh_1[i, :num_neigh_1[i]], cutoff_func)
+                                s, etypes_neigh_1[i, :num_neigh_1[i]],
+                                cutoff_func)
 
             # kernel is nonzero only if central atoms are of the same species
             if c2 == etypes1[i]:
@@ -3024,7 +3036,8 @@ def many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, n
 
             # Calculate many-body descriptor value for j
             qjs[j] = q_value_mc(neigh_dists_2[j, :num_neigh_2[j]], r_cut,
-                                s, etypes_neigh_2[j, :num_neigh_2[j]], cutoff_func)
+                                s, etypes_neigh_2[j, :num_neigh_2[j]],
+                                cutoff_func)
 
             # kernel is nonzero only if central atoms are of the same species
             if c1 == etypes2[j]:
@@ -3032,7 +3045,8 @@ def many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, n
 
         for i in range(bond_array_1.shape[0]):
             for j in range(bond_array_2.shape[0]):
-                # kernel is nonzero only if central atoms are of the same species
+                # kernel is nonzero only if central atoms are of the same
+                # species
                 if etypes1[i] == etypes2[j]:
                     kij = k_sq_exp_double_dev(qis[i], qjs[j], sig, ls)
                 else:
@@ -3046,9 +3060,12 @@ def many_body_mc_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, n
 
 
 @njit
-def many_body_mc_grad_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists_2, num_neigh_1,
-                          num_neigh_2, c1, c2, etypes1, etypes2, etypes_neigh_1, etypes_neigh_2,
-                          species1, species2, d1, d2, sig, ls, r_cut, cutoff_func):
+def many_body_mc_grad_jit(bond_array_1, bond_array_2, neigh_dists_1,
+                          neigh_dists_2, num_neigh_1,
+                          num_neigh_2, c1, c2, etypes1, etypes2,
+                          etypes_neigh_1, etypes_neigh_2,
+                          species1, species2, d1, d2, sig, ls, r_cut,
+                          cutoff_func):
     """gradient of many-body multi-element kernel between two force components
     w.r.t. the hyperparameters, accelerated with Numba.
 
@@ -3061,18 +3078,18 @@ def many_body_mc_grad_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists
             of neighbours for the atoms in the first local environment.
         neigh_dists_2 (np.ndarray): matrix padded with zero values of distances
             of neighbours for the atoms in the second local environment.
-        num_neigh_1 (np.ndarray): number of neighbours of each atom in the first
-            local environment
-        num_neigh_2 (np.ndarray): number of neighbours of each atom in the second
-            local environment
+        num_neigh_1 (np.ndarray): number of neighbours of each atom in the
+            first local environment
+        num_neigh_2 (np.ndarray): number of neighbours of each atom in the
+            second local environment
         c1 (int): atomic species of the central atom in env 1
         c2 (int): atomic species of the central atom in env 2
         etypes1 (np.ndarray): atomic species of atoms in env 1
         etypes2 (np.ndarray): atomic species of atoms in env 2
-        etypes_neigh_1 (np.ndarray): atomic species of atoms in the neighbourhoods
-            of atoms in env 1
-        etypes_neigh_2 (np.ndarray): atomic species of atoms in the neighbourhoods
-            of atoms in env 2
+        etypes_neigh_1 (np.ndarray): atomic species of atoms in the
+            neighbourhoods of atoms in env 1
+        etypes_neigh_2 (np.ndarray): atomic species of atoms in the
+            neighbourhoods of atoms in env 2
         species1 (np.ndarray): all the atomic species present in trajectory 1
         species2 (np.ndarray): all the atomic species present in trajectory 2
         d1 (int): Force component of the first environment.
@@ -3135,7 +3152,8 @@ def many_body_mc_grad_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists
 
             # Calculate many-body descriptor value for i
             qis[i] = q_value_mc(neigh_dists_1[i, :num_neigh_1[i]], r_cut,
-                                s, etypes_neigh_1[i, :num_neigh_1[i]], cutoff_func)
+                                s, etypes_neigh_1[i, :num_neigh_1[i]],
+                                cutoff_func)
 
             # ki2s[i] = k_sq_exp_double_dev(qis[i], q2, sig, ls)
             if c2 == etypes1[i]:
@@ -3158,7 +3176,8 @@ def many_body_mc_grad_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists
 
             # Calculate many-body descriptor value for j
             qjs[j] = q_value_mc(neigh_dists_2[j, :num_neigh_2[j]], r_cut,
-                                s, etypes_neigh_2[j, :num_neigh_2[j]], cutoff_func)
+                                s, etypes_neigh_2[j, :num_neigh_2[j]],
+                                cutoff_func)
 
             # k1js[j] = k_sq_exp_double_dev(q1, qjs[j], sig, ls)
 
@@ -3204,11 +3223,13 @@ def many_body_mc_grad_jit(bond_array_1, bond_array_2, neigh_dists_1, neigh_dists
 
 
 @njit
-def many_body_mc_force_en_jit(bond_array_1, bond_array_2, neigh_dists_1, num_neigh_1,
+def many_body_mc_force_en_jit(bond_array_1, bond_array_2, neigh_dists_1,
+                              num_neigh_1,
                               c1, c2, etypes1, etypes2, etypes_neigh_1,
-                              species1, species2, d1, sig, ls, r_cut, cutoff_func):
-    """many-body many-element kernel between force and energy components accelerated
-    with Numba.
+                              species1, species2, d1, sig, ls, r_cut,
+                              cutoff_func):
+    """many-body many-element kernel between force and energy components
+        accelerated with Numba.
 
     Args:
         bond_array_1 (np.ndarray): many-body bond array of the first local
@@ -3217,14 +3238,14 @@ def many_body_mc_force_en_jit(bond_array_1, bond_array_2, neigh_dists_1, num_nei
             environment.
         neigh_dists_1 (np.ndarray): matrix padded with zero values of distances
             of neighbours for the atoms in the first local environment.
-        num_neigh_1 (np.ndarray): number of neighbours of each atom in the first
-            local environment
+        num_neigh_1 (np.ndarray): number of neighbours of each atom in the
+            first local environment
         c1 (int): atomic species of the central atom in env 1
         c2 (int): atomic species of the central atom in env 2
         etypes1 (np.ndarray): atomic species of atoms in env 1
         etypes2 (np.ndarray): atomic species of atoms in env 2
-        etypes_neigh_1 (np.ndarray): atomic species of atoms in the neighbourhoods
-            of atoms in env 1
+        etypes_neigh_1 (np.ndarray): atomic species of atoms in the
+            neighbourhoods of atoms in env 1
         species1 (np.ndarray): all the atomic species present in trajectory 1
         species2 (np.ndarray): all the atomic species present in trajectory 2
         d1 (int): Force component of the first environment.
@@ -3272,7 +3293,8 @@ def many_body_mc_force_en_jit(bond_array_1, bond_array_2, neigh_dists_1, num_nei
 
             # Calculate many-body descriptor value for i
             qis[i] = q_value_mc(neigh_dists_1[i, :num_neigh_1[i]], r_cut,
-                                s, etypes_neigh_1[i, :num_neigh_1[i]], cutoff_func)
+                                s, etypes_neigh_1[i, :num_neigh_1[i]],
+                                cutoff_func)
 
             if c2 == etypes1[i]:
                 ki2s[i] = k_sq_exp_dev(qis[i], q2, sig, ls)
