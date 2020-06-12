be7c057b29fc734f42defd93b2478ba31ffca453
Steven Torrisi <stevetorr@users.noreply.github.com>
2020-06-12 12:44:11 -0400

    Add, test method to remove force data from a gp (#183)
    * Add, test method to remove data from a gp
    
    * Update requirements to try to fix bug with pymatgen
    
    * Added set_seed argument
    
    Can merge later with refactory. Adds more flexibility.
    
    * Reduce cutoff in unit test
    
    * Added pop functionality to remove data method

--- tests/test_gp.py
+++ tests/test_gp.py
@@ -9,6 +9,7 @@ from pytest import raises
 from scipy.optimize import OptimizeResult
 
 import flare
+from flare.predict import predict_on_structure
 from flare.gp import GaussianProcess
 from flare.env import AtomicEnvironment
 from flare.struc import Structure
@@ -18,7 +19,6 @@ from flare.otf_parser import OtfAnalysis
 
 from .fake_gp import generate_hm, get_tstp, get_random_structure
 from copy import deepcopy
-
 multihyps_list = [True, False]
 
 
@@ -370,6 +370,77 @@ def test_training_statistics():
     assert len(data['species']) == len(set(test_structure.coded_species))
     assert len(data['envs_by_species']) == len(set(
         test_structure.coded_species))
+
+
+def test_remove_force_data():
+    """
+    Train a GP on one fake structure. Store forces from prediction.
+    Add a new fake structure and ensure predictions change; then remove
+    the structure and ensure predictions go back to normal.
+    :return:
+    """
+
+    test_structure, forces = get_random_structure(np.eye(3),
+                                                  ['H', 'Be'],
+                                                  5)
+
+    test_structure_2, forces_2 = get_random_structure(np.eye(3),
+                                                  ['H', 'Be'],
+                                                  5)
+
+    gp = GaussianProcess(kernel_name='2', cutoffs=[2],
+                         name='remove_tester')
+
+    gp.update_db(test_structure, forces)
+
+    with raises(ValueError):
+        gp.remove_force_data(1000000)
+
+    init_forces, init_stds = predict_on_structure(test_structure, gp,
+                                                 write_to_structure=False)
+    init_forces_2, init_stds_2 = predict_on_structure(test_structure_2, gp,
+                                                 write_to_structure=False)
+
+    # Alternate adding in the entire structure and adding in only one atom.
+    for custom_range in [None, [0]]:
+
+        # Add in data and ensure the predictions change in reponse
+        gp.update_db(test_structure_2, forces_2, custom_range=custom_range)
+
+        new_forces, new_stds = predict_on_structure(test_structure, gp,
+                                                    write_to_structure=False)
+
+        new_forces_2, new_stds_2 = predict_on_structure(test_structure_2, gp,
+                                                     write_to_structure=False)
+
+        assert not np.array_equal(init_forces, new_forces)
+        assert not np.array_equal(init_forces_2, new_forces_2)
+        assert not np.array_equal(init_stds, new_stds)
+        assert not np.array_equal(init_stds_2, new_stds_2)
+
+        # Remove that data and test to see that the predictions revert to
+        # what they were previously
+        if custom_range == [0]:
+            popped_strucs, popped_forces = gp.remove_force_data(5)
+        else:
+            popped_strucs, popped_forces = gp.remove_force_data([5, 6, 7, 8,
+                                                                 9])
+
+        for i in range(len(popped_forces)):
+            assert np.array_equal(popped_forces[i],forces_2[i])
+            assert np.array_equal(popped_strucs[i].structure.positions,
+                              test_structure_2.positions)
+
+        final_forces, final_stds = predict_on_structure(test_structure, gp,
+                                                     write_to_structure=False)
+        final_forces_2, final_stds_2 = predict_on_structure(test_structure_2, gp,
+                                                     write_to_structure=False)
+
+        assert np.array_equal(init_forces, final_forces)
+        assert np.array_equal(init_stds, final_stds)
+
+        assert np.array_equal(init_forces_2, final_forces_2)
+        assert np.array_equal(init_stds_2, final_stds_2)
 
 
 class TestHelper():
