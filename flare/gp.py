294f0cd9ff8aa503e3e03ff6675f38b9227e1c55
Steven Torrisi <stevetorr@users.noreply.github.com>
2020-07-07 16:58:56 -0400

    Update gp.py

--- flare/gp.py
+++ flare/gp.py
@@ -230,7 +230,6 @@ class GaussianProcess:
         
         if hyps_mask is not None:
             self.hyps_mask = hyps_mask
-            if 
             if self.cutoffs!= hyps_mask['cutoffs']:
                 self.adjust_cutoffs(hyps_mask['cutoffs'],train = False,
                                    new_hyps_mask = hyps_mask)
