fe21344b5ed8011099ef05d8aae1b67e793ba27a
Yu Shelby Xie <xieyu.fish@gmail.com>
2020-07-11 13:44:23 -0400

    rm comment lines

--- flare/ase/dft.py
+++ flare/ase/dft.py
@@ -39,12 +39,4 @@ def run_dft_par(atoms, structure, dft_calc, **dft_kwargs):
     # The results will be written to the 'results' dictionary, and structure.forces
     # directly return the atoms.calc.results['forces']
 
-    #    # Write energy, forces, and stress to structure.
-    #    structure.potential_energy = energy
-    #    structure.stress = stress
-    #    structure.stds = np.zeros((structure.nat, 3))
-    #    structure.stress_stds = None
-    #    structure.partial_stress_stds = None
-    #    structure.local_energy_stds = None
-
     return forces
