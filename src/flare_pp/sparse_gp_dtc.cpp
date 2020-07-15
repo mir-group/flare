bfba1e441361c410e525d861bd26c49c987a9ba2
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-07-15 18:35:06 -0400

    finish first draft of DTC variance

--- src/flare_pp/sparse_gp_dtc.cpp
+++ src/flare_pp/sparse_gp_dtc.cpp
@@ -6,7 +6,7 @@ SparseGP_DTC ::SparseGP_DTC(std::vector<Kernel *> kernels, double sigma_e,
                             double sigma_f, double sigma_s)
     : SparseGP(kernels, sigma_e, sigma_f, sigma_s) {}
 
-void SparseGP_DTC ::update_matrices(){
+void SparseGP_DTC ::update_matrices() {
   // Combine Kuf_struc and Kuf_env.
   int n_sparse = Kuf_struc.rows();
   int n_struc_labels = Kuf_struc.cols();
@@ -40,9 +40,10 @@ void SparseGP_DTC ::update_matrices(){
   alpha = Sigma * Kuf * noise.asDiagonal() * y;
 }
 
-void SparseGP_DTC ::predict_DTC(StructureDescriptor test_structure,
-    Eigen::VectorXd & mean_vector, Eigen::VectorXd & variance_vector,
-    std::vector<Eigen::VectorXd> & mean_contributions){
+void SparseGP_DTC ::predict_DTC(
+    StructureDescriptor test_structure, Eigen::VectorXd &mean_vector,
+    Eigen::VectorXd &variance_vector,
+    std::vector<Eigen::VectorXd> &mean_contributions) {
 
   int n_atoms = test_structure.noa;
   int n_out = 1 + 3 * n_atoms + 6;
@@ -51,8 +52,8 @@ void SparseGP_DTC ::predict_DTC(StructureDescriptor test_structure,
 
   // Store kernel matrices for each kernel.
   std::vector<Eigen::MatrixXd> kern_mats;
-  for (int i = 0; i < n_kernels; i ++){
-      kern_mats.push_back(Eigen::MatrixXd::Zero(n_out, n_sparse));
+  for (int i = 0; i < n_kernels; i++) {
+    kern_mats.push_back(Eigen::MatrixXd::Zero(n_out, n_sparse));
   }
   Eigen::MatrixXd kern_mat = Eigen::MatrixXd::Zero(n_out, n_sparse);
 
@@ -67,21 +68,28 @@ void SparseGP_DTC ::predict_DTC(StructureDescriptor test_structure,
   }
 
   // Sum the kernels.
-  for (int i = 0; i < n_kernels; i ++){
-      kern_mat += kern_mats[i];
+  for (int i = 0; i < n_kernels; i++) {
+    kern_mat += kern_mats[i];
   }
 
   // Compute mean contributions and total mean prediction.
+  mean_contributions = std::vector<Eigen::VectorXd>{};
+  for (int i = 0; i < n_kernels; i++) {
+    mean_contributions.push_back(kern_mats[i] * alpha);
+  }
+
   mean_vector = kern_mat * alpha;
 
   // Compute variances.
-  Eigen::VectorXd Q_self, K_self = Eigen::VectorXd::Zero(n_out);
+  Eigen::VectorXd V_SOR, Q_self, K_self = Eigen::VectorXd::Zero(n_out);
 
   // Note: Calculation of the self-kernel can be parallelized.
-  for (int i = 0; i < n_kernels; i ++){
-      K_self += kernels[i]->self_kernel_struc(test_structure);
+  for (int i = 0; i < n_kernels; i++) {
+    K_self += kernels[i]->self_kernel_struc(test_structure);
   }
 
-  // TODO next: Calculate Q_self. Note that only the trace is needed of the
-  // full matrix product is needed.
+  Q_self = (kern_mat * Kuu_inverse * kern_mat.transpose()).diagonal();
+  V_SOR = (kern_mat * Sigma * kern_mat.transpose()).diagonal();
+
+  variance_vector = K_self - Q_self + V_SOR;
 }
