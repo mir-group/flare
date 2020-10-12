4c20825f8bd54c04da57b84fb871efb20a6f0ea3
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-10-12 11:00:06 -0400

    update cluster descriptor

--- src/flare_pp/v2/descriptors/compact_descriptor.cpp
+++ src/flare_pp/v2/descriptors/compact_descriptor.cpp
@@ -10,3 +10,48 @@
 CompactDescriptor::CompactDescriptor() {}
 
 DescriptorValues::DescriptorValues() {}
+
+ClusterDescriptor::ClusterDescriptor() {}
+
+void ClusterDescriptor ::add_cluster(const DescriptorValues &structure){
+
+  // If this is the first time adding a cluster, initialize attributes.
+  if (n_clusters == 0) {
+    n_types = structure.n_types;
+    n_descriptors = structure.n_descriptors;
+
+    Eigen::MatrixXd empty_mat;
+    Eigen::VectorXd empty_vec;
+    for (int s = 0; s < n_types; s++) {
+      descriptors.push_back(empty_mat);
+      descriptor_norms.push_back(empty_vec);
+      type_count.push_back(0);
+      cumulative_type_count.push_back(0);
+    }
+  }
+
+  // Resize descriptor matrices.
+  for (int s = 0; s < n_types; s++) {
+    descriptors[s].conservativeResize(
+      type_count[s] + structure.n_atoms_by_type[s], n_descriptors);
+    descriptor_norms[s].conservativeResize(
+      type_count[s] + structure.n_atoms_by_type[s]);
+  }
+
+  // Update descriptors.
+  for (int s = 0; s < n_types; s++) {
+    for (int i = 0; i < structure.n_atoms_by_type[s]; i++){
+      descriptors[s].row(type_count[s] + i) = structure.descriptors[s].row(i);
+      descriptor_norms[s](type_count[s] + i) = structure.descriptor_norms[s](i);
+    }
+  }
+
+  // Update type counts.
+  for (int s = 0; s < n_types; s++) {
+    type_count[s] += structure.n_atoms_by_type[s];
+    n_clusters += structure.n_atoms_by_type[s];
+    if (s > 0)
+      cumulative_type_count[s] =
+        cumulative_type_count[s - 1] + type_count[s - 1];
+  }
+}
--- src/flare_pp/v2/descriptors/compact_descriptor.h
+++ src/flare_pp/v2/descriptors/compact_descriptor.h
@@ -9,6 +9,7 @@ class CompactStructure;
 class DescriptorValues;
 
 // Descriptor calculator.
+// TODO: Rename to DescriptorCalculator.
 class CompactDescriptor {
 public:
   CompactDescriptor();
@@ -16,6 +17,7 @@ public:
   virtual DescriptorValues compute_struc(CompactStructure &structure) = 0;
 };
 
+// TODO: Rename to StructureDescriptor.
 class DescriptorValues {
 public:
   DescriptorValues();
@@ -29,7 +31,25 @@ public:
   std::vector<Eigen::VectorXd> descriptor_norms, descriptor_force_dots;
   std::vector<Eigen::VectorXi> neighbor_counts, cumulative_neighbor_counts,
       atom_indices, neighbor_indices;
+
+  // TODO: Rename to n_clusters_by_type.
   std::vector<int> n_atoms_by_type, n_neighbors_by_type;
+};
+
+class ClusterDescriptor{
+public:
+  ClusterDescriptor();
+
+  std::vector<Eigen::MatrixXd> descriptors;
+  std::vector<Eigen::VectorXd> descriptor_norms;
+  std::vector<int> type_count, cumulative_type_count;
+  int n_descriptors, n_types;
+  int n_clusters = 0;
+
+  // Add all clusters in a structure.
+  void add_cluster(const DescriptorValues &structure);
+
+  // TODO: Allow specific clusters to be added.
 };
 
 #endif
