3e811e51169299fd6caf93655d60241d045fbe0a
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-10-22 22:59:53 -0400

    add three body type count

--- src/flare_pp/v2/descriptors/three_body.cpp
+++ src/flare_pp/v2/descriptors/three_body.cpp
@@ -40,39 +40,37 @@ DescriptorValues ThreeBody ::compute_struc(CompactStructure &structure){
 #pragma omp parallel for
   for (int i = 0; i < desc.n_atoms; i++) {
     int i_species = structure.species[i];
+    int t1 = desc.n_types -
+      (n_species - i_species) * (n_species - i_species + 1) *
+      (n_species - i_species + 2) / 6;
     int i_neighbors = structure.neighbor_count(i);
     int rel_index = structure.cumulative_neighbor_count(i);
     for (int j = 0; j < i_neighbors; j++) {
       int j_species = structure.species[j];
+      if (j_species < i_species) continue;
+      int t2 = (n_species - i_species) * (n_species - i_species + 1) / 2 -
+        (n_species - j_species) * (n_species - j_species + 1) / 2;
       int neigh_index_1 = rel_index + j;
       double r1 = structure.relative_positions(neigh_index_1, 0);
-      if (r1 <= cutoff) continue;
+      if (r1 > cutoff) continue;
       double x1 = structure.relative_positions(neigh_index_1, 1);
       double y1 = structure.relative_positions(neigh_index_1, 2);
       double z1 = structure.relative_positions(neigh_index_1, 3);
       for (int k = 0; k < i_neighbors; k++){
         if (j == k) continue;
         int k_species = structure.species[k];
+        if (k_species < j_species) continue;
+        int t3 = k_species - j_species;
         int neigh_index_2 = rel_index + k;
         double r2 = structure.relative_positions(neigh_index_2, 0);
-        if (r2 <= cutoff) continue;
+        if (r2 > cutoff) continue;
         double x2 = structure.relative_positions(neigh_index_2, 1);
         double y2 = structure.relative_positions(neigh_index_2, 2);
         double z2 = structure.relative_positions(neigh_index_2, 3);
-        if ((j_species >= i_species) && (k_species >= i_species)){
-          double r3 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1, 2));
-          if (r3 <= cutoff) continue;
-//         int species_diff = j_species - i_species;
-//         int current_type =
-//           i_species * n_species - (i_species * (i_species - 1)) / 2 +
-//           species_diff;
-//         double r = structure.relative_positions(neigh_index, 0);
-//         // Check that atom is within descriptor cutoff.
-//         if (r <= cutoff) {
-//           // Update type count.
-//           type_count(current_type)++;
-//         }
-        }
+        double r3 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1, 2));
+        if (r3 > cutoff) continue;
+        int current_type = t1 + t2 + t3;
+        type_count(current_type)++;
       }
     }
   }
