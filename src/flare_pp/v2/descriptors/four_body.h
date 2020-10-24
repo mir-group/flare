a18cb71c3eb2bacb7138f1af797122fa338419f8
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-10-24 16:33:36 -0400

    start four body descriptor

---
+++ src/flare_pp/v2/descriptors/four_body.h
@@ -0,0 +1,30 @@
+#ifndef FOUR_BODY_H
+#define FOUR_BODY_H
+
+#include <vector>
+#include <string>
+#include "compact_descriptor.h"
+#include "compact_structure.h"
+
+class CompactStructure;
+
+class FourBody : public CompactDescriptor {
+public:
+  double cutoff;
+  int n_species;
+
+  std::function<void(std::vector<double> &, double, double,
+                     std::vector<double>)>
+      cutoff_function;
+  std::string cutoff_name;
+  std::vector<double> cutoff_hyps;
+
+  FourBody();
+  FourBody(double cutoff, int n_species,
+                const std::string &cutoff_name,
+                const std::vector<double> &cutoff_hyps);
+
+  DescriptorValues compute_struc(CompactStructure &structure);
+};
+
+#endif
