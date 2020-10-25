c3f9faa8f94e170ec2b2593444caaa14431f46e6
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2020-10-25 12:25:36 -0400

    add four body counts

--- src/flare_pp/v2/descriptors/four_body.cpp
+++ src/flare_pp/v2/descriptors/four_body.cpp
@@ -29,77 +29,86 @@ DescriptorValues FourBody ::compute_struc(CompactStructure &structure){
   // Initialize descriptor values.
   DescriptorValues desc = DescriptorValues();
 
-//   desc.n_descriptors = 3;
-//   desc.n_types = n_species * (n_species + 1) * (n_species + 2) / 6;
-//   desc.n_atoms = structure.noa;
-//   desc.volume = structure.volume;
-//   int n_neighbors = structure.n_neighbors;
+  desc.n_descriptors = 6;
+  desc.n_types =
+    n_species * (n_species + 1) * (n_species + 2) * (n_species + 3) /
+    (2 * 3 * 4);
+  desc.n_atoms = structure.noa;
+  desc.volume = structure.volume;
+  int n_neighbors = structure.n_neighbors;
 
-//   // Count types.
-//   Eigen::VectorXi type_count = Eigen::VectorXi::Zero(desc.n_types);
-//   // TODO: Consider parallelizing.
-//   for (int i = 0; i < desc.n_atoms; i++) {
-//     int i_species = structure.species[i];
-//     int t1 = desc.n_types -
-//       (n_species - i_species) * (n_species - i_species + 1) *
-//       (n_species - i_species + 2) / 6;
-//     int i_neighbors = structure.neighbor_count(i);
-//     int rel_index = structure.cumulative_neighbor_count(i);
-//     for (int j = 0; j < i_neighbors; j++) {
-//       int neigh_index_1 = rel_index + j;
-//       int j_species = structure.neighbor_species(neigh_index_1);
-//       if (j_species < i_species) continue;
-//       int t2 = (n_species - i_species) * (n_species - i_species + 1) / 2 -
-//         (n_species - j_species) * (n_species - j_species + 1) / 2;
-//       double r1 = structure.relative_positions(neigh_index_1, 0);
-//       if (r1 > cutoff) continue;
-//       double x1 = structure.relative_positions(neigh_index_1, 1);
-//       double y1 = structure.relative_positions(neigh_index_1, 2);
-//       double z1 = structure.relative_positions(neigh_index_1, 3);
-//       for (int k = 0; k < i_neighbors; k++){
-//         if (j == k) continue;
-//         int neigh_index_2 = rel_index + k;
-//         int k_species = structure.neighbor_species(neigh_index_2);
-//         if (k_species < j_species) continue;
-//         int t3 = k_species - j_species;
-//         double r2 = structure.relative_positions(neigh_index_2, 0);
-//         if (r2 > cutoff) continue;
-//         double x2 = structure.relative_positions(neigh_index_2, 1);
-//         double y2 = structure.relative_positions(neigh_index_2, 2);
-//         double z2 = structure.relative_positions(neigh_index_2, 3);
-//         double r3 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1, 2));
-//         int current_type = t1 + t2 + t3;
-//         type_count(current_type)++;
-//       }
-//     }
-//   }
+  // Count types.
+  Eigen::VectorXi type_count = Eigen::VectorXi::Zero(desc.n_types);
+  // TODO: Consider parallelizing.
+  for (int i = 0; i < desc.n_atoms; i++) {
+    int i_species = structure.species[i];
+    int t1 = desc.n_types -
+      (n_species - i_species) * (n_species - i_species + 1) *
+      (n_species - i_species + 2) * (n_species - i_species + 3) / 24;
+    int i_neighbors = structure.neighbor_count(i);
+    int rel_index = structure.cumulative_neighbor_count(i);
+    // First loop over neighbors.
+    for (int j = 0; j < i_neighbors; j++) {
+      int neigh_index_1 = rel_index + j;
+      int j_species = structure.neighbor_species(neigh_index_1);
+      if (j_species < i_species) continue;
+      int t2 = (n_species - i_species) * (n_species - i_species + 1) *
+        (n_species - i_species + 2) / 6 -
+        (n_species - j_species) * (n_species - j_species + 1) *
+        (n_species - j_species + 2) / 6;
+      double r1 = structure.relative_positions(neigh_index_1, 0);
+      if (r1 > cutoff) continue;
+      // Second loop over neighbors.
+      for (int k = 0; k < i_neighbors; k++){
+        if (j == k) continue;
+        int neigh_index_2 = rel_index + k;
+        int k_species = structure.neighbor_species(neigh_index_2);
+        if (k_species < j_species) continue;
+        int t3 = (n_species - j_species) * (n_species - j_species + 1) / 2 -
+          (n_species - k_species) * (n_species - k_species + 1) / 2;
+        double r2 = structure.relative_positions(neigh_index_2, 0);
+        if (r2 > cutoff) continue;
+        // Third loop over neighbors.
+        for (int l = 0; l < i_neighbors; l++){
+          if ((j == l) || (k == l)) continue;
+          int neigh_index_3 = rel_index + l;
+          int l_species = structure.neighbor_species(neigh_index_3);
+          if (l_species < k_species) continue;
+          int t4 = l_species - k_species;
+          double r3 = structure.relative_positions(neigh_index_3, 0);
+          if (r3 > cutoff) continue;
+          int current_type = t1 + t2 + t3 + t4;
+          type_count(current_type)++;
+        }
+      }
+    }
+  }
 
-//   // Initialize arrays.
-//   for (int s = 0; s < desc.n_types; s++) {
-//     int n_s = type_count(s);
-//     int n_neigh = n_s * 2;
-//     int n_d = 3;
+  // Initialize arrays.
+  for (int s = 0; s < desc.n_types; s++) {
+    int n_s = type_count(s);
+    int n_neigh = n_s * 3;
+    int n_d = 6;
 
-//     // Record species and neighbor count.
-//     // (For the 3-body descriptor, there are 2 neighbors.)
-//     desc.n_atoms_by_type.push_back(n_s);
-//     desc.n_neighbors_by_type.push_back(n_neigh);
+    // Record species and neighbor count.
+    desc.n_atoms_by_type.push_back(n_s);
+    desc.n_neighbors_by_type.push_back(n_neigh);
 
-//     desc.descriptors.push_back(Eigen::MatrixXd::Zero(n_s, n_d));
-//     desc.descriptor_force_dervs.push_back(
-//       Eigen::MatrixXd::Zero(n_neigh * 3, n_d));
-//     desc.neighbor_coordinates.push_back(Eigen::MatrixXd::Zero(n_neigh, 3));
+    desc.descriptors.push_back(Eigen::MatrixXd::Zero(n_s, n_d));
+    desc.descriptor_force_dervs.push_back(
+      Eigen::MatrixXd::Zero(n_neigh * 3, n_d));
+    desc.neighbor_coordinates.push_back(Eigen::MatrixXd::Zero(n_neigh, 3));
 
-//     desc.descriptor_norms.push_back(Eigen::VectorXd::Zero(n_s));
-//     desc.descriptor_force_dots.push_back(Eigen::VectorXd::Zero(n_neigh * 3));
-//     desc.cutoff_values.push_back(Eigen::VectorXd::Zero(n_s));
-//     desc.cutoff_dervs.push_back(Eigen::VectorXd::Zero(n_neigh * 3));
+    desc.descriptor_norms.push_back(Eigen::VectorXd::Zero(n_s));
+    desc.descriptor_force_dots.push_back(Eigen::VectorXd::Zero(n_neigh * 3));
+    desc.cutoff_values.push_back(Eigen::VectorXd::Zero(n_s));
+    desc.cutoff_dervs.push_back(Eigen::VectorXd::Zero(n_neigh * 3));
 
-//     desc.neighbor_counts.push_back(Eigen::VectorXi::Zero(n_s));
-//     desc.cumulative_neighbor_counts.push_back(Eigen::VectorXi::Zero(n_s));
-//     desc.atom_indices.push_back(Eigen::VectorXi::Zero(n_s));
-//     desc.neighbor_indices.push_back(Eigen::VectorXi::Zero(n_neigh));
-//   }
+    desc.neighbor_counts.push_back(Eigen::VectorXi::Zero(n_s));
+    desc.cumulative_neighbor_counts.push_back(Eigen::VectorXi::Zero(n_s));
+    desc.atom_indices.push_back(Eigen::VectorXi::Zero(n_s));
+    desc.neighbor_indices.push_back(Eigen::VectorXi::Zero(n_neigh));
+  }
 
 //   // Store descriptors.
 //   Eigen::VectorXi type_counter = Eigen::VectorXi::Zero(desc.n_types);
