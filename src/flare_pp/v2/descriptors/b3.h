a264859e95f1ca1dd486fa34df245429c2036928
ClaudioZeni <claudiopuntozeni@gmail.com>
2020-11-05 17:02:13 +0100

    First draft of B3 descriptor

---
+++ src/flare_pp/v2/descriptors/b3.h
@@ -0,0 +1,56 @@
+#ifndef B3_H
+#define B3_H
+
+#include <vector>
+#include <string>
+#include "compact_descriptor.h"
+
+class CompactStructure;
+
+class B3 : public CompactDescriptor {
+public:
+  std::function<void(std::vector<double> &, std::vector<double> &, double, int,
+                     std::vector<double>)>
+      radial_pointer;
+  std::function<void(std::vector<double> &, double, double,
+                     std::vector<double>)>
+      cutoff_pointer;
+  std::string radial_basis, cutoff_function;
+  std::vector<double> radial_hyps, cutoff_hyps;
+  std::vector<int> descriptor_settings;
+
+  B3();
+
+  B3(const std::string &radial_basis,
+                const std::string &cutoff_function,
+                const std::vector<double> &radial_hyps,
+                const std::vector<double> &cutoff_hyps,
+                const std::vector<int> &descriptor_settings);
+
+  DescriptorValues compute_struc(CompactStructure &structure);
+};
+
+void compute_B3(
+    Eigen::MatrixXd &B3_vals, Eigen::MatrixXd &B3_force_dervs,
+    Eigen::VectorXd &B3_norms, Eigen::VectorXd &B3_force_dots,
+    const Eigen::MatrixXd &single_bond_vals,
+    const Eigen::MatrixXd &single_bond_force_dervs,
+    const Eigen::VectorXi &unique_neighbor_count,
+    const Eigen::VectorXi &cumulative_neighbor_count,
+    const Eigen::VectorXi &descriptor_indices, int nos, int N, int lmax);
+
+void compute_single_bond(
+    Eigen::MatrixXd &single_bond_vals, Eigen::MatrixXd &force_dervs,
+    Eigen::MatrixXd &neighbor_coordinates,
+    Eigen::VectorXi &neighbor_count, Eigen::VectorXi &cumulative_neighbor_count,
+    Eigen::VectorXi &neighbor_indices,
+    std::function<void(std::vector<double> &, std::vector<double> &, double,
+                       int, std::vector<double>)>
+        radial_function,
+    std::function<void(std::vector<double> &, double, double,
+                       std::vector<double>)>
+        cutoff_function,
+    int nos, int N, int lmax, const std::vector<double> &radial_hyps,
+    const std::vector<double> &cutoff_hyps, const CompactStructure &structure);
+
+#endif
---
+++ src/flare_pp/v2/descriptors/wigner3j.h
@@ -0,0 +1,13 @@
+#ifndef wigner3j
+#define wigner3j
+#include <Eigen/Dense>
+
+#include <vector>
+#include <string>
+
+extern const Eigen::MatrixXd &w1;
+extern const Eigen::MatrixXd &w2;
+extern const Eigen::MatrixXd &w3;
+extern const Eigen::MatrixXd &w4;
+
+#endif
