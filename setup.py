c07b5342084e619dfee3d7b22ca46d7ab0c9e3aa
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2021-03-09 15:39:33 -0500

    update setup

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ class CMakeBuild(build_ext):
 setuptools.setup(
     name="flare_pp",
     packages=setuptools.find_packages(exclude=["tests"]),
-    version="0.0.1",
+    version="0.0.2",
     author="Materials Intelligence Research",
     author_email="mir@g.harvard.edu",
     description="Many-body extension of the flare code",
