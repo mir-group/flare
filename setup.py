2350fc9e697cadf2efc40e3096d333235bd10a40
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2021-03-09 19:54:35 -0500

    make the code pip installable

--- setup.py
+++ setup.py
@@ -160,8 +160,8 @@ class CMakeBuild(build_ext):
 
 setuptools.setup(
     name="flare_pp",
-    packages=setuptools.find_packages(exclude=["tests"]),
-    version="0.0.4",
+    packages=setuptools.find_packages(exclude=[]),
+    version="0.0.18",
     author="Materials Intelligence Research",
     author_email="mir@g.harvard.edu",
     description="Many-body extension of the flare code",
