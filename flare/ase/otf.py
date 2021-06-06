f7a694965f3f0c83c17a77d99e32c3d3ec83f286
YuuuXie <xieyu.fish@gmail.com>
2021-06-06 15:09:20 -0400

    fix ase degraded set_calculator

--- flare/ase/otf.py
+++ flare/ase/otf.py
@@ -106,7 +106,7 @@ class ASE_OTF(OTF):
 
         self.atoms = FLARE_Atoms.from_ase_atoms(atoms)
         if calculator is not None:
-            self.atoms.set_calculator(calculator)
+            self.atoms.calc = calculator
         self.timestep = timestep
         self.md_engine = md_engine
         self.md_kwargs = md_kwargs
