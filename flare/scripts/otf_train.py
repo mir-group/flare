d3d10ed3fd921aa62f0f99cf294ef25c8ddae852
YuuuXie <xieyu.fish@gmail.com>
2022-01-14 11:24:56 -0500

    use get and set default values

--- flare/scripts/otf_train.py
+++ flare/scripts/otf_train.py
@@ -29,18 +29,28 @@ with open(sys.argv[1], "r") as f:
     config = yaml.safe_load(f)
 
 # -------------- Set up Supercell from ASE Atoms --------------
-super_cell = io.read(config["ase_atoms"]["file"], format=config["ase_atoms"]["format"])
-super_cell *= config["ase_atoms"]["replicate"]
+# parse parameters
+atoms_config = config["ase_atoms"]
+atoms_file = atoms_config.get("file")
+atoms_format = atoms_config.get("format")
+replicate = ase_config.get("replicate", [1, 1, 1])
+jitter = atoms_config.gt("jitter", 0)
+
+super_cell = io.read(atoms_file, format=atoms_format)
+super_cell *= replicate
 
 # jitter positions to give nonzero force on first frame
 for atom_pos in super_cell.positions:
     for coord in range(3):
-        atom_pos[coord] += (2 * np.random.random() - 1) * config["ase_atoms"]["jitter"]
+        atom_pos[coord] += (2 * np.random.random() - 1) * jitter
 
 # ----------------- Set up ASE DFT calculator -----------------
 # find the module including the ASE DFT calculator class by name
-dft_module_name = ""
 ase_dft_calc_name = config["ase_dft_calc"]["name"]
+ase_dft_calc_kwargs = config["ase_dft_calc"]["kwargs"]
+ase_dft_calc_params = config["ase_dft_calc"]["params"]
+
+dft_module_name = ""
 for importer, modname, ispkg in pkgutil.iter_modules(ase_calculators.__path__):
     module_info = pyclbr.readmodule("ase.calculators." + modname)
     if ase_dft_calc_name in module_info:
@@ -52,42 +62,51 @@ dft_calc = None
 dft_module = importlib.import_module("ase.calculators." + dft_module_name)
 for name, obj in inspect.getmembers(dft_module, inspect.isclass):
     if name == ase_dft_calc_name:
-        dft_calc = obj(**config["ase_dft_calc"]["kwargs"])
-dft_calc.set(**config["ase_dft_calc"]["params"])
+        dft_calc = obj(**ase_dft_calc_kwargs)
+dft_calc.set(**ase_dft_calc_params)
 
 # ---------- create gaussian process model -------------------
 flare_config = config["ase_flare_calc"]
+kernels = flare_config.get("kernels")
+random_init_hyps = flare_config.get("random_init_hyps", True)
+gp_parameters = flare_config.get("gp_parameters")
+opt_algorithm = flare_config.get("opt_algorithm", "BFGS")
+n_cpus = flare_config.get("n_cpus", 1)
+use_mapping = flare_config.get("use_mapping", False)
+if use_mapping:
+    grid_params = flare_config.get("grid_params")
+    vap_map = flare_config.get("var_map", "pca")
 
 # set up GP hyperparameters
 pm = ParameterHelper(
-    kernels=flare_config["kernels"],
-    random=flare_config["random_init_hyps"],
-    parameters=flare_config["gp_parameters"],
+    kernels=kernels,
+    random=random_init_hyps,
+    parameters=gp_parameters,
 )
 hm = pm.as_dict()
 
 gp_model = GaussianProcess(
-    kernels=flare_config["kernels"],
+    kernels=kernels,
     component="mc",
     hyps=hm["hyps"],
     cutoffs=hm["cutoffs"],
     hyp_labels=hm["hyp_labels"],
-    opt_algorithm=flare_config["opt_algorithm"],
-    n_cpus=flare_config["n_cpus"],
+    opt_algorithm=opt_algorithm,
+    n_cpus=n_cpus,
 )
 
 # ----------- create mapped gaussian process ------------------
-if flare_config["use_mapping"]:
+if use_mapping:
     unique_species = []
     for s in super_cell.symbols:
         if s not in unique_species:
             unique_species.append(s)
     coded_unique_species = symbols2numbers(unique_species)
     mgp_model = MappedGaussianProcess(
-        grid_params=flare_config["grid_params"],
+        grid_params=grid_params,
         unique_species=coded_unique_species,
-        n_cpus=flare_config["n_cpus"],
-        var_map=flare_config["var_map"],
+        n_cpus=n_cpus,
+        var_map=var_map,
     )
 else:
     mgp_model = None
@@ -96,13 +115,14 @@ else:
 flare_calc = FLARE_Calculator(
     gp_model=gp_model,
     mgp_model=mgp_model,
-    par=flare_config["par"],
-    use_mapping=flare_config["use_mapping"],
+    par=n_cpus > 1,
+    use_mapping=use_mapping,
 )
 
 # On-the-fly training engine
 # intialize velocity
-MaxwellBoltzmannDistribution(super_cell, config["otf"]["temperature"] * units.kB)
+temperature = config["otf"].get("temperature", 0)
+MaxwellBoltzmannDistribution(super_cell, temperature * units.kB)
 Stationary(super_cell)  # zero linear momentum
 ZeroRotation(super_cell)  # zero angular momentum
 
@@ -110,7 +130,7 @@ test_otf = OTF(
     super_cell,
     flare_calc=flare_calc,
     dft_calc=dft_calc,
-    **config["otf"],
+    **config.get("otf"),
 )
 
 # run on-the-fly training
