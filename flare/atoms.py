760ff516f8d37dd5f2a4690ac3f3895c9d86adeb
Yu Xie <xieyu.fish@gmail.com>
2022-05-28 16:40:12 -0400

    assign None to properties when empty list

--- flare/atoms.py
+++ flare/atoms.py
@@ -79,7 +79,10 @@ class FLARE_Atoms(Atoms):
 
     @property
     def potential_energy(self):
-        return self.get_potential_energy()
+        if "energy" in self.calc.implemented_properties:
+            return self.get_potential_energy()
+        else:
+            return None
 
     @potential_energy.setter
     def potential_energy(self, energy_label):
@@ -98,12 +101,17 @@ class FLARE_Atoms(Atoms):
 
     @property
     def stress(self):
-        return self.get_stress()
+        if "stress" in self.calc.implemented_properties:
+            return self.get_stress()
+        else:
+            return None
 
     @stress.setter
     def stress(self, stress_array):
         if (stress_array is None) or (len(stress_array) == 6):
             self.label_setter("stress", stress_array)
+        elif len(stress_array) == 0:
+            self.label_setter("stress", None)
         else:
             raise ValueError("stress_array should be None or array of length 6")
 
