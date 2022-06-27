fix variance sorting in sparse_gp.cpp

flare/bffs/sgp/calculator.py
@@ -107,15 +107,14 @@ class SGP_Calculator(Calculator):
         # TODO: Generalize this variance type to multiple descriptors.
         elif self.gp_model.variance_type == "local":
             variances = structure_descriptor.local_uncertainties[0]
-            sorted_variances = sort_variances(structure_descriptor, variances)
-            stds = np.zeros(len(sorted_variances))
-            for n in range(len(sorted_variances)):
-                var = sorted_variances[n]
+            stds = np.zeros(len(variances))
+            for n in range(len(variances)):
+                var = variances[n]
                 if var > 0:
                     stds[n] = np.sqrt(var)
                 else:
                     stds[n] = -np.sqrt(np.abs(var))
-            stds_full = np.zeros((len(sorted_variances), 3))
+            stds_full = np.zeros((len(variances), 3))
 
             # Divide by the signal std to get a unitless value.
             stds_full[:, 0] = stds / np.abs(self.gp_model.hyps[0])
@@ -183,24 +182,3 @@ class SGP_Calculator(Calculator):
             self.gp_model.sparse_gp.write_sparse_descriptors(
                 f"sparse_desc_{filename}", contributor
             )
-
-
-def sort_variances(structure_descriptor, variances):
-    # Check that the variance length matches the number of atoms.
-    assert len(variances) == structure_descriptor.noa
-    sorted_variances = np.zeros(len(variances))
-
-    # Sort the variances by atomic order.
-    descriptor_values = structure_descriptor.descriptors[0]
-    atom_indices = descriptor_values.atom_indices
-    n_types = descriptor_values.n_types
-    assert n_types == len(atom_indices)
-
-    v_count = 0
-    for s in range(n_types):
-        for n in range(len(atom_indices[s])):
-            atom_index = atom_indices[s][n]
-            sorted_variances[atom_index] = variances[v_count]
-            v_count += 1
-
-    return sorted_variances