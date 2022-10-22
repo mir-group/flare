1ebdc2afbeb0a8cee64d1a83f59e1b2a26422ed9
Yu Xie <xieyu.fish@gmail.com>
2022-10-22 18:59:07 -0400

    add dot product kernel to lammps

--- lammps_plugins/pair_flare.cpp
+++ lammps_plugins/pair_flare.cpp
@@ -129,7 +129,7 @@ void PairFLARE::compute(int eflag, int vflag) {
                   single_bond_vals, n_species, n_max, l_max);
 
     compute_energy_and_u(B2_vals, B2_norm_squared, single_bond_vals, power,
-           n_species, n_max, l_max, beta_matrices[itype - 1], u, &evdwl);
+           n_species, n_max, l_max, beta_matrices[itype - 1], u, &evdwl, normalized);
 
     // Continue if the environment is empty.
     if (B2_norm_squared < empty_thresh)
@@ -260,7 +260,7 @@ double PairFLARE::init_one(int i, int j) {
 
 void PairFLARE::read_file(char *filename) {
   int me = comm->me;
-  char line[MAXLINE], radial_string[MAXLINE], cutoff_string[MAXLINE];
+  char line[MAXLINE], radial_string[MAXLINE], cutoff_string[MAXLINE], kernel_string[MAXLINE];
   int radial_string_length, cutoff_string_length;
   FILE *fptr;
 
@@ -279,7 +279,8 @@ void PairFLARE::read_file(char *filename) {
     fgets(line, MAXLINE, fptr); // Date and contributor
 
     fgets(line, MAXLINE, fptr); // Power, use integer instead of double for simplicity
-    sscanf(line, "%i", &power);
+    sscanf(line, "%i %s", &power, &kernel_string);
+    kernel_string_length = strlen(kernel_string);
 
     fgets(line, MAXLINE, fptr);
     sscanf(line, "%s", radial_string); // Radial basis set
@@ -303,6 +304,7 @@ void PairFLARE::read_file(char *filename) {
   MPI_Bcast(&cutoff_string_length, 1, MPI_INT, 0, world);
   MPI_Bcast(radial_string, radial_string_length + 1, MPI_CHAR, 0, world);
   MPI_Bcast(cutoff_string, cutoff_string_length + 1, MPI_CHAR, 0, world);
+  MPI_Bcast(kernel_string, kernel_string_length + 1, MPI_CHAR, 0, world);
 
   // Parse the cutoffs.
   int n_cutoffs = n_species * n_species;
@@ -351,6 +353,13 @@ void PairFLARE::read_file(char *filename) {
     cutoff_function = quadratic_cutoff;
   else if (!strcmp(cutoff_string, "cosine"))
     cutoff_function = cos_cutoff;
+
+  // Set the kernel
+  if (!strcmp(kernel_string, "NormalizedDotProduct")) {
+    normalized = true;
+  } else {
+    normalized = false;
+  }
 
   // Parse the beta vectors.
   memory->create(beta, beta_size * n_species, "pair:beta");
