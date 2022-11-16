10210e1bef7a40061b28af2fcebaabb47651503d
Yu Xie <xieyu.fish@gmail.com>
2022-11-16 11:47:49 -0500

    add util to write embed coeffs for lammps use

--- flare/utils/__init__.py
+++ flare/utils/__init__.py
@@ -3,6 +3,8 @@ Utility functions for various tasks.
 """
 from json import JSONEncoder
 import numpy as np
+from datetime import datetime
+from math import ceil
 
 
 class NumpyEncoder(JSONEncoder):
@@ -44,3 +46,29 @@ class NumpyEncoder(JSONEncoder):
         elif isinstance(obj, (np.ndarray,)):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
+
+
+def write_embed_coeffs(filename, embed_coeffs, n_species, n_max, l_max, contributor):
+    assert embed_coeffs.shape[0] % 2 == 0
+    assert embed_coeffs.shape[1] == n_species * n_max * (l_max + 1)
+
+    now = datetime.now()
+    d_embed = embed_coeffs.shape[0] // 2
+
+    with open(filename, "w") as f:
+        f.write(f"DATE: {now}. CONTRIBUTOR: {contributor}\n")
+        f.write(f"{d_embed}\n")
+        embed_array = np.reshape(embed_coeffs, -1)
+        n_cols = 5
+        n_rows = len(embed_array) // n_cols
+        n_residual = len(embed_array) % n_cols
+        
+        # before residual
+        coeff_body = np.reshape(embed_array[:n_rows * n_cols], (n_rows, n_cols))
+        coeff_str = "\n".join([" ".join([str(l) for l in line]) for line in coeff_body])
+
+        # residual
+        coeff_residual = embed_array[-n_residual:]
+        coeff_str += "\n" + " ".join([str(l) for l in coeff_residual])
+
+        f.write(coeff_str)
