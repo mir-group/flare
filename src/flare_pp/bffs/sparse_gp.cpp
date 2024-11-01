47431da892d3762d8bef25844d5ad06e12206994
Jonathan Vandermause <jonathan_vandermause@g.harvard.edu>
2024-11-01 00:59:27 -0400

    use runtime error instead

--- src/flare_pp/bffs/sparse_gp.cpp
+++ src/flare_pp/bffs/sparse_gp.cpp
@@ -191,7 +191,9 @@ void SparseGP ::add_specific_environments(const Structure &structure,
 void SparseGP ::add_uncertain_environments(const Structure &structure,
                                            const std::vector<int> &n_added) {
 
-  assert(n_added.size() == n_kernels && "n_added must have the same size as the number of kernels");
+    if (n_added.size() != n_kernels) {
+        throw std::runtime_error("n_added must have the same size as the number of kernels");
+    }
 
   initialize_sparse_descriptors(structure);
   // Compute cluster uncertainties.
